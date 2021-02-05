da1152b25c993073b9771a41eb424282f94fb08b
topsmart20 <oswellgrundmeier42@gmail.com>
2021-02-05 09:48:27 +0100

    modified header design

--- prmApp/src/components/Enquiries.vue
+++ prmApp/src/components/Enquiries.vue
@@ -80,8 +80,8 @@
     font-family: "Avenir", Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased; 
     -moz-osx-font-smoothing: grayscale; 
-    color: #2c3e50;
-    /*text-align: center;
+    /*color: #2c3e50;
+    text-align: center;
     margin-top: 60px;*/
     }
 #app .title {
--- prmApp/src/components/Header.vue
+++ prmApp/src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container-fluid">
+    <div class="container-fluid-header">
         <div class="header">
             <div class="header-body">
                 <div class="row align-items-center">
@@ -17,17 +17,17 @@
 
                         <div class="avatar-group">
                             <div class="avatar avatar-xs">
-                                <img src="../assets/img/Icon ionic-md-notifications.png" alt="Obvestila" class="avatar-img">
+                                <img src="../assets/img/bell.png" alt="Obvestila" class="avatar-img">
                             </div>
                             <div class="avatar avatar-xs">
-                                <img src="../assets/img/settings.svg" alt="Nastavitve" class="avatar-img ">
+                                <img src="../assets/img/settings.png" alt="Nastavitve" class="avatar-img ">
                             </div>
                         </div>
                         <div>
                             <h3 class="header-title navbar-text" id="userName">Dr. Ime Priimek</h3>
                         </div>
-                        <div class="btn btn-lg btn-rounded-circle" data-toggle="modal">
-                            <img src="../assets/img/user.svg" alt="Uporabnik " class="avatar-img">
+                        <div class="btn btn-md btn-rounded-circle" data-toggle="modal">
+                            <img src="../assets/img/user1.png" alt="Uporabnik " class="avatar-img">
                         </div>
                     </div>
                 </div>
