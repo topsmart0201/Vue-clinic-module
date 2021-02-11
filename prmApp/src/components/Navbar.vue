916806d5d687b9f2d732c6f293192ff0fd735d5f
topsmart20 <oswellgrundmeier42@gmail.com>
2021-02-11 17:09:03 +0100

    Ikone v menuju

--- prmApp/src/components/Navbar.vue
+++ prmApp/src/components/Navbar.vue
@@ -3,61 +3,41 @@
     <div class="container-fluid-navbar navbar-margin-top">
     <nav class="navbar navbar-vertical fixed-left navbar-expand-md navbar-light">
          <ul class="nav flex-column">
-            <li class="nav-item">
-                <a v-bind:class="[linkClass, (currentPage.menu === 'Domov') ? selectedLinkClass : '']" href="#" @click="setActive('Domov', 'Domov', true)">Domov</a>
-            </li>
-            <li class="nav-item">
-                <a v-bind:class="[linkClass, (currentPage.menu === 'Koledar') ? selectedLinkClass : '']" href="#" @click="setActive('Koledar', 'Koledar', true)">Koledar</a>
-            </li>            
-            <li class="nav-item">
-                <a v-bind:class="[linkClass, (currentPage.menu === 'Pacienti') ? selectedLinkClass : '']" href="#" @click="setActive('Pacienti', 'Pacienti', true)">Pacienti</a>
-            </li>
-            <li class="nav-item">
-                <a v-bind:class="[linkClass, (currentPage.menu === 'Dokumenti') ? selectedLinkClass : '']" href="#" @click="setActive('Dokumenti', 'Racuni', true)">Dokumenti</a>
-            </li>
-            <li v-if="currentPage.menu === 'Dokumenti' && this.currentPage.opened" class="nav-item">
-                <a class="nav-submenu" v-bind:class="[linkClass, (currentPage.submenu === 'Racuni') ? selectedLinkClass : '']" href="#" @click="setActive('Dokumenti', 'Racuni', false)">Računi</a>
-            </li>
-            <li v-if="currentPage.menu === 'Dokumenti' && this.currentPage.opened" class="nav-item">
-                <a class="nav-submenu" v-bind:class="[linkClass, (currentPage.submenu === 'Predracuni') ? selectedLinkClass : '']" href="#" @click="setActive('Dokumenti', 'Predracuni', false)">Predračuni</a>
-            </li>
-            <li v-if="currentPage.menu === 'Dokumenti' && this.currentPage.opened" class="nav-item">
-                <a class="nav-submenu" v-bind:class="[linkClass, (currentPage.submenu === 'AvansniRacuni') ? selectedLinkClass : '']" href="#" @click="setActive('Dokumenti', 'AvansniRacuni', false)">Avansni računi</a>
-            </li>
-            <li class="nav-item">
-                <a v-bind:class="[linkClass, (currentPage.menu === 'Naloge') ? selectedLinkClass : '']" href="#" @click="setActive('Naloge', 'Naloge', true)">Naloge</a>
-            </li>
-            <li class="nav-item">
-                <a v-bind:class="[linkClass, (currentPage.menu === 'Statistika') ? selectedLinkClass : '']" href="#" @click="setActive('Statistika', 'StatistikaKlinike', true)">Statistika</a>
-            </li>
-            <li v-if="currentPage.menu === 'Statistika' && this.currentPage.opened" class="nav-item">
+            <a v-bind:class="[linkClass, (currentPage.menu === 'Domov') ? selectedLinkClass : '']" href="#" @click="setActive('Domov', 'Domov', true)"><b-icon icon="house"/>Domov</a>
+            <a v-bind:class="[linkClass, (currentPage.menu === 'Koledar') ? selectedLinkClass : '']" href="#" @click="setActive('Koledar', 'Koledar', true)"><b-icon icon="calendar-date"/>Koledar</a>           
+            <a v-bind:class="[linkClass, (currentPage.menu === 'Pacienti') ? selectedLinkClass : '']" href="#" @click="setActive('Pacienti', 'Pacienti', true)"><b-icon icon="people"/>Pacienti</a>
+            <a v-bind:class="[linkClass, (currentPage.menu === 'Dokumenti') ? selectedLinkClass : '']" href="#" @click="setActive('Dokumenti', 'Racuni', true)"><b-icon icon="journals"/>Dokumenti</a>
+            <div v-if="currentPage.menu === 'Dokumenti' && this.currentPage.opened" class="nav-item">
+                <a class="nav-submenu" v-bind:class="[linkClass, (currentPage.submenu === 'Racuni') ? selectedLinkClass : '']" href="#" @click="setActive('Dokumenti', 'Racuni', false)"><b-icon icon="journal-check"/>Računi</a>
+                <a class="nav-submenu" v-bind:class="[linkClass, (currentPage.submenu === 'Predracuni') ? selectedLinkClass : '']" href="#" @click="setActive('Dokumenti', 'Predracuni', false)"><b-icon icon="journal-text"/>Predračuni</a>
+                <a class="nav-submenu" v-bind:class="[linkClass, (currentPage.submenu === 'AvansniRacuni') ? selectedLinkClass : '']" href="#" @click="setActive('Dokumenti', 'AvansniRacuni', false)"><b-icon icon="journal-plus"/>Avansni računi</a>
+            </div>
+            <a v-bind:class="[linkClass, (currentPage.menu === 'Naloge') ? selectedLinkClass : '']" href="#" @click="setActive('Naloge', 'Naloge', true)"><b-icon icon="map"/>Naloge</a>
+            <a v-bind:class="[linkClass, (currentPage.menu === 'Statistika') ? selectedLinkClass : '']" href="#" @click="setActive('Statistika', 'StatistikaKlinike', true)"><b-icon icon="bar-chart"/>Statistika</a>
+            <div v-if="currentPage.menu === 'Statistika' && this.currentPage.opened" class="nav-item">
                 <a class="nav-submenu" v-bind:class="[linkClass, (currentPage.submenu === 'StatistikaKlinike') ? selectedLinkClass : '']" href="#" @click="setActive('Statistika', 'StatistikaKlinike', false)">Statistika klinike</a>
-            </li>
-            <li v-if="currentPage.menu === 'Statistika' && this.currentPage.opened" class="nav-item">
-                <a class="nav-submenu" v-bind:class="[linkClass, (currentPage.submenu === 'OsebnaStatistika') ? selectedLinkClass : '']" href="#" @click="setActive('Statistika', 'OsebnaStatistika', false)">Osebna statistika</a>
-            </li>
-            <li class="nav-item">
-                <a v-bind:class="[linkClass, (currentPage.menu === 'PorocilaInObracuni') ? selectedLinkClass : '']" href="#" @click="setActive('PorocilaInObracuni', 'ObracunEmazing', true)">Poročila in obračuni</a>
-            </li>
-            <li v-if="currentPage.menu === 'PorocilaInObracuni' && this.currentPage.opened" class="nav-item">
+                <a class="nav-submenu" v-bind:class="[linkClass, (currentPage.submenu === 'OsebnaStatistika') ? selectedLinkClass : '']" href="#" @click="setActive('Statistika', 'OsebnaStatistika', false)"><b-icon icon="person"/>Osebna statistika</a>
+            </div>
+            <a v-bind:class="[linkClass, (currentPage.menu === 'PorocilaInObracuni') ? selectedLinkClass : '']" href="#" @click="setActive('PorocilaInObracuni', 'ObracunEmazing', true)"><b-icon icon="reception-4"/>Poročila in obračuni</a>
+            <div v-if="currentPage.menu === 'PorocilaInObracuni' && this.currentPage.opened" class="nav-item">
                 <a class="nav-submenu" v-bind:class="[linkClass, (currentPage.submenu === 'ObracunEmazing') ? selectedLinkClass : '']" href="#" @click="setActive('PorocilaInObracuni', 'ObracunEmazing', false)">Obračun - Emazing</a>
-            </li>
-            <li v-if="currentPage.menu === 'PorocilaInObracuni' && this.currentPage.opened" class="nav-item">
                 <a class="nav-submenu" v-bind:class="[linkClass, (currentPage.submenu === 'ObracunDoktor') ? selectedLinkClass : '']" href="#" @click="setActive('PorocilaInObracuni', 'ObracunDoktor', false)">Obračun - doktor</a>
-            </li>
-            <li class="nav-item">
-                <a v-bind:class="[linkClass, (currentPage.menu === 'Nastavitve') ? selectedLinkClass : '']" href="#" @click="setActive('Nastavitve', 'StoritveInIzdelki', true)">Nastavitve</a>
-            </li>
-            <li v-if="currentPage.menu === 'Nastavitve' && this.currentPage.opened" class="nav-item">
-                <a class="nav-submenu" v-bind:class="[linkClass, (currentPage.submenu === 'StoritveInIzdelki') ? selectedLinkClass : '']" href="#" @click="setActive('Nastavitve', 'StoritveInIzdelki', false)">Storitve in izdelki</a>
-            </li>
-            <li v-if="currentPage.menu === 'Nastavitve' && this.currentPage.opened" class="nav-item">
+            </div>
+            <a v-bind:class="[linkClass, (currentPage.menu === 'Nastavitve') ? selectedLinkClass : '']" href="#" @click="setActive('Nastavitve', 'StoritveInIzdelki', true)"><b-icon icon="gear"/>Nastavitve</a>
+            <div v-if="currentPage.menu === 'Nastavitve' && this.currentPage.opened" class="nav-item">
+                <a class="nav-submenu" v-bind:class="[linkClass, (currentPage.submenu === 'StoritveInIzdelki') ? selectedLinkClass : '']" href="#" @click="setActive('Nastavitve', 'StoritveInIzdelki', false)"><b-icon icon="palette"/>Storitve in izdelki</a>
                 <a class="nav-submenu" v-bind:class="[linkClass, (currentPage.submenu === 'Uporabniki') ? selectedLinkClass : '']" href="#" @click="setActive('Nastavitve', 'Uporabniki', false)">Uporabniki</a>
-            </li>            
+            </div>            
         </ul>
     </nav> 
     </div>   
 </template>
+
+<style>
+.b-icon {
+    margin-right: 10px;
+}
+</style>
 
 <script>
 
