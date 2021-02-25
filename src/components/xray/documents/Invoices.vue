e4746c5d2bb0f0a10bcfc03d9fe776438221ac8a
topsmart20 <oswellgrundmeier42@gmail.com>
2021-02-25 23:31:41 +0530

    added documents components

---
+++ src/components/xray/documents/Invoices.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <h1>Invoices</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  name: 'Invoices',
+  data: function () {
+    return {
+    }
+  },
+  methods: {
+  },
+  mounted () {
+  }
+}
+</script>
+<template>
+    <div class="container">
+        <h1>Invoices</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  name: 'Invoices',
+  data: function () {
+    return {
+    }
+  },
+  methods: {
+  },
+  mounted () {
+  }
+}
+</script>
---
+++ src/components/xray/documents/Offers.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <h1>Offers</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  name: 'Offers',
+  data: function () {
+    return {
+    }
+  },
+  methods: {
+  },
+  mounted () {
+  }
+}
+</script>
