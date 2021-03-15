150d498939fa4e92ac8b824999506c1779d34453
topsmart20 <oswellgrundmeier42@gmail.com>
2021-03-16 01:47:02 +0200

    cleanup

--- newAssignment-SmartPRM/SmartPRM-ClinicModule/src/plugins/global.js
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash'
-import Vue from 'vue'
-
-const components = require.context('../components/xray')
-_.forEach(components.keys(), (fileName) => {
-  const componentConfig = components(fileName)
-  const componentName = fileName.split('/').pop().split('.')[0]
-  Vue.component(componentName, componentConfig.default || componentConfig)
-})
