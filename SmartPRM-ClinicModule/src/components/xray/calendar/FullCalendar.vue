2a633b2c0894525c54e785025c3d3568f4badab5
topsmart20 <oswellgrundmeier42@gmail.com>
2021-03-19 22:35:24 +0530

    Added column resource view for fullcalendar

--- SmartPRM-ClinicModule/src/components/xray/calendar/FullCalendar.vue
+++ SmartPRM-ClinicModule/src/components/xray/calendar/FullCalendar.vue
@@ -1,4 +1,5 @@
 <template>
+<b-container fluid>
   <VueFullCalendar
   :defaultView="calendarOptions.defaultView"
   :plugins="calendarOptions.plugins"
@@ -7,10 +8,11 @@
   :header="{
       left: 'prev,next today',
       center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,resourceTimeGridDay'
+      right: 'dayGridMonth,/*timeGridWeek,timeGridDay*/,resourceTimeGridWeek,resourceTimeGridDay'
     }"
-  @eventClick="eventData(args)"
+  @eventClick="eventData"
   />
+</b-container>
 </template>
 <script>
 import VueFullCalendar from '@fullcalendar/vue'
@@ -27,38 +29,47 @@ export default {
   },
   data () {
     return {
+      modalShow: false,
       calendarOptions: {
         plugins: [dayGridPlugin, listPlugin, timeGridPlugin, interactionPlugin, resourceTimeGrid],
         timeZone: 'UTC',
         defaultView: 'dayGridMonth',
         resources: [
-          { id: 'a', title: 'Resource A' },
-          { id: 'b', title: 'Resource B' },
-          { id: 'c', title: 'Resource C' },
-          { id: 'd', title: 'Resource D' }
+          { id: 'a', title: 'Resource A', eventColor: 'sandybrown' },
+          { id: 'b', title: 'Resource B', eventColor: 'blue' },
+          { id: 'c', title: 'Resource C', eventColor: 'red' },
+          { id: 'd', title: 'Resource D', eventColor: 'black' }
         ],
-        events: 'https://fullcalendar.io/demo-events.json?with-resources=4&single-day'
-        // events: { title: 'Event Now', start: new Date(), resourceId: 'a', color: 'black' }
+        // events: 'https://fullcalendar.io/demo-events.json?with-resources=4&single-day'
+        events: [
+          { title: 'Event 1', start: '2021-03-04T08:00:00', end: '2021-03-04T09:00:00', allDay: 'false', resourceId: 'a' },
+          { title: 'Event 1.1', start: '2021-03-05T08:00:00', end: '2021-03-05T10:00:00', allDay: 'false', resourceId: 'a' },
+          { title: 'Event 2', start: '2021-03-07T09:00:00', end: '2021-03-07T10:00:00', allDay: 'false', resourceId: 'b' },
+          { title: 'Event 3', start: '2021-03-05T12:00:00', end: '2021-03-05T01:00:00', allDay: 'false', resourceId: 'c' },
+          { title: 'Event 4', start: '2021-03-08T10:00:00', end: '2021-03-08T11:00:00', allDay: 'false', resourceId: 'd' }
+        ]
       }
     }
   },
   methods: {
-    eventData (args) {
-      console.log('ARGS:', args)
+    eventData () {
+      console.log('before alert', this.modalShow)
+      this.modalShow = true
       alert('Hii')
+      console.log('after alert', this.modalShow)
     }
   }
 }
 </script>
 
 <style lang="scss">
-.fc-event, .fc-event:hover{
-    color: #050505 !important;
-    background-color: rgb(250, 112, 0);
-  }
-  .fc-license-message{
-    display:none;
-  }
+.fc-event{
+  color: white !important;
+  border: none !important;
+}
+.fc-license-message{
+  display:none;
+}
   @import '~@fullcalendar/core/main.css';
   @import '~@fullcalendar/daygrid/main.css';
   @import '~@fullcalendar/timegrid/main.css';
