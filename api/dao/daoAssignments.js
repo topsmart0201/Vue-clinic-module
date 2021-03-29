4d103b1532aba872491d9c9cfb4cc66a68ce7e63
topsmart20 <oswellgrundmeier42@gmail.com>
2021-03-29 15:44:54 +0200

    Personal dentist - FK in enquiries table

--- api/dao/daoAssignments.js
+++ api/dao/daoAssignments.js
@@ -10,9 +10,10 @@ const pool = new Pool({
 
 const getAssignments = (request, response, scope, userid) =>  {
     if (scope == "All") {
-        var statement = ["SELECT todos.*, enquiries.name AS patientname, enquiries.last_name AS patientlastname, users.name AS todoname", 
-                         "FROM todos JOIN enquiries ON todos.enquiry_id = enquiries.id",
-                         "JOIN users ON todos.user_id = users.id",
+        var statement = ["SELECT todos.*, enquiries.name AS patientname, enquiries.last_name AS patientlastname, users.name AS todoname, dentists.name AS dentistname FROM todos",
+                         "LEFT JOIN enquiries ON todos.enquiry_id = enquiries.id",
+                         "LEFT JOIN users ON todos.user_id = users.id",
+                         "LEFT JOIN users dentists ON enquiries.prm_dentist_user_id = users.id",
                          "WHERE ( (todos.due_at = now()::date) OR (todos.due_at < now()::date AND todos.completed=false) )",
                          "ORDER BY todos.due_at ASC"].join('\n') 
         pool.query(statement, (error, results) => {
@@ -22,11 +23,26 @@ const getAssignments = (request, response, scope, userid) =>  {
             response.status(200).json(results.rows)
         })
     } else if (scope == "Self") {
-        var statement = ["SELECT todos.*, enquiries.name AS patientname, enquiries.last_name AS patientlastname, users.name AS todoname", 
-                         "FROM todos JOIN enquiries ON todos.enquiry_id = enquiries.id",
-                         "JOIN users ON todos.user_id = users.id",
+        var statement = ["SELECT todos.*, enquiries.name AS patientname, enquiries.last_name AS patientlastname, users.name AS todoname, dentists.name AS dentistname FROM todos",
+                         "LEFT JOIN enquiries ON todos.enquiry_id = enquiries.id",
+                         "LEFT JOIN users ON todos.user_id = users.id",
+                         "LEFT JOIN users dentists ON enquiries.prm_dentist_user_id = users.id",
                          "WHERE ( (todos.due_at = now()::date) OR (todos.due_at < now()::date AND todos.completed=false) )",
                          "AND users.id = $1",
+                         "ORDER BY todos.due_at ASC"].join('\n') 
+        pool.query(statement, [userid], (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).json(results.rows)
+        })  
+    } else if (scope == "AllNurses") {
+        var statement = ["SELECT todos.*, enquiries.name AS patientname, enquiries.last_name AS patientlastname, users.name AS todoname, dentists.name AS dentistname FROM todos",
+                         "LEFT JOIN enquiries ON todos.enquiry_id = enquiries.id",
+                         "LEFT JOIN users ON todos.user_id = users.id",
+                         "LEFT JOIN users dentists ON enquiries.prm_dentist_user_id = users.id",
+                         "WHERE ( (todos.due_at = now()::date) OR (todos.due_at < now()::date AND todos.completed=false) )",
+                         "AND ( users.prm_role_id = (SELECT role_id FROM prm_role where role_name = 'Nurse') OR users.prm_role_id = (SELECT role_id FROM prm_role where role_name = 'SuperNurse') )",
                          "ORDER BY todos.due_at ASC"].join('\n') 
         pool.query(statement, [userid], (error, results) => {
             if (error) {
