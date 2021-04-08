48e8ca358a3b5f79b7d545580c0769da35d1da7d
topsmart20 <oswellgrundmeier42@gmail.com>
2021-04-08 13:52:36 +0200

    further work done on 'assignments' page

--- SmartPRM-ClinicModule/src/views/Dashboards/Dashboard2.vue
+++ SmartPRM-ClinicModule/src/views/Dashboards/Dashboard2.vue
@@ -1,78 +1,165 @@
 <template>
-    <b-container>
-        <b-row style="justify-content: center">
+    <b-container fluid>
+        <b-row style="justify-content: center;">
             <b-col lg="12">
                 <iq-card class-name="iq-card-block iq-card-stretch iq-card-height">
                     <template v-slot:headerTitle>
-                        <h4 class="card-title">Overdue assignments</h4>
+                        <h4 class="card-title">My assignments</h4>
                     </template>
-                    <template v-slot:body>
-                        <b-row>
-                            <b-col md="12" class="table-responsive table-borderless">
-                                <b-table id="my-overdue-assignments"
-                                         thead-class="d-none"
-                                         hover
-                                         :items="items"
-                                         :fields="columns"
-                                         :per-page="perPage"
-                                         :current-page="currentPage">
-                                    <template v-slot:cell(completed)="data">
-                                        <b-checkbox v-model="data.item.completed" :disabled="item.disabled" name="check-button" inline :key="index">
-                                        </b-checkbox>
-                                    </template>
-                                    <template v-slot:cell(description)="data">
-                                        {{ data.item.description }}
-                                    </template>
-                                    <template v-slot:cell(fullname)="data">
-                                        {{ data.item.patientname }} {{ data.item.patientlastname }}
-                                    </template>
-                                    <template v-slot:cell(due_at)="data">
-                                        {{ data.item.due_at }}
-                                    </template>
-                                </b-table>
-                            </b-col>
-                        </b-row>
-                    </template>
-                    <!--<template v-slot:headerTitle>
-                        <h4 class="card-title">My Assignments</h4>
-                    </template>
-                    <template v-slot:body>
-                        <b-row>
-                            <b-col md="12" class="table-responsive table-borderless">
-                                <b-table id="my-assignments"
-                                         thead-class="d-none"
-                                         hover
-                                         :items="items"
-                                         :fields="columns"
-                                         :per-page="perPage"
-                                         :current-page="currentPage">
-                                    <template v-slot:cell(completed)="data">
-                                        <b-checkbox v-model="data.item.completed" :disabled="item.disabled" name="check-button" inline :key="index">
-                                        </b-checkbox>
-                                    </template>
-                                    <template v-slot:cell(description)="data">
-                                        {{ data.item.description }}
-                                    </template>
-                                    <template v-slot:cell(fullname)="data">
-                                        {{ data.item.patientname }} {{ data.item.patientlastname }}
-                                    </template>
-                                    <template v-slot:cell(due_at)="data">
-                                        {{ data.item.due_at }}
-                                    </template>
-                                </b-table>
-                            </b-col>
-                        </b-row>
-                    </template>-->
+                    <iq-card class-name="iq-card-block iq-card-stretch iq-card-height">
+                        <template v-slot:headerTitle>
+                            <h5>Overdue assignments</h5>
+                        </template>
+                        <template v-slot:body>
+                            <b-list-group class="list-group-flush">
+                                <b-list-group-item v-for="item in items.slice(0, 5)" :key="item.id">
+                                    <div class="assignments-container">
+                                        <b-checkbox v-model="item.completed" :disabled="item.disabled" name="check-button" inline :key="index" class="completed-assignment"></b-checkbox>
+                                        <span>{{ item.description }}</span>
+                                        <span class="pl-5">{{ item.patientname }} {{ item.patientlastname }}</span>
+                                        <span class="pl-5">Dr. Zobo Zdravnik</span>
+                                        <span class="text-right">{{ item.due_at }}</span>
+                                    </div>
+                                </b-list-group-item>
+                            </b-list-group>
+                        </template>
+                    </iq-card>
+                    <iq-card class-name="iq-card-block iq-card-stretch iq-card-height">
+                        <template v-slot:headerTitle>
+                            <h5>Today's assignments</h5>
+                        </template>
+                        <template v-slot:body>
+                            <b-list-group class="list-group-flush">
+                                <b-list-group-item v-for="item in items.slice(10, 13)" :key="item.id">
+                                    <div class="assignments-container">
+                                        <b-checkbox v-model="item.completed" :disabled="item.disabled" name="check-button" inline :key="index" class="completed-assignment"></b-checkbox>
+                                        <span>{{ item.description }}</span>
+                                        <span class="pl-5">{{ item.patientname }} {{ item.patientlastname }}</span>
+                                        <span class="pl-5">Dr. Zobo Zdravnik</span>
+                                        <span class="text-right">{{ item.due_at }}</span>
+                                    </div>
+                                </b-list-group-item>
+                            </b-list-group>
+                        </template>
+                    </iq-card>
+                    <iq-card class-name="iq-card-block iq-card-stretch iq-card-height">
+                        <template v-slot:headerTitle>
+                            <h5>Future assignments</h5>
+                        </template>
+                        <template v-slot:body>
+                            <b-list-group class="list-group-flush">
+                                <b-list-group-item v-for="item in items.slice(23, 29)" :key="item.id">
+                                    <div class="assignments-container">
+                                        <b-checkbox v-model="item.completed" :disabled="item.disabled" name="check-button" inline :key="index" class="completed-assignment"></b-checkbox>
+                                        <span>{{ item.description }}</span>
+                                        <span class="pl-5">{{ item.patientname }} {{ item.patientlastname }}</span>
+                                        <span class="pl-5">Dr. Zobo Zdravnik</span>
+                                        <span class="text-right">{{ item.due_at }}</span>
+                                    </div>
+                                </b-list-group-item>
+                            </b-list-group>
+                        </template>
+                    </iq-card>
+                    <iq-card class-name="iq-card-block iq-card-stretch iq-card-height">
+                        <template v-slot:headerTitle>
+                            <h5>Completed assignments</h5>
+                        </template>
+                        <template v-slot:body>
+                            <b-list-group class="list-group-flush">
+                                <b-list-group-item v-for="item in items.slice(33, 49)" :key="item.id">
+                                    <div class="assignments-container">
+                                            <b-checkbox v-model="item.completed" :disabled="item.disabled" name="check-button" inline :key="index" class="completed-assignment"></b-checkbox>
+                                            <span>{{ item.description }}</span>
+                                            <span class="pl-5">{{ item.patientname }} {{ item.patientlastname }}</span>
+                                            <span class="pl-5">Dr. Zobo Zdravnik</span>
+                                            <span class="text-right">{{ item.due_at }}</span>
+                                    </div>
+                                </b-list-group-item>
+                            </b-list-group>
+                        </template>
+                    </iq-card>
                 </iq-card>
-        </b-col>
+            </b-col>
         </b-row>
     </b-container>
 </template>
+<!--<b-container>
+    <b-row style="justify-content: center">
+        <b-col lg="12">
+            <iq-card class-name="iq-card-block iq-card-stretch iq-card-height">
+                <template v-slot:headerTitle>
+                    <h4 class="card-title">Overdue assignments</h4>
+                </template>
+                <template v-slot:body>
+                    <b-row>
+                        <b-col md="12" class="table-responsive table-borderless">
+                            <b-table id="my-overdue-assignments"
+                                     thead-class="d-none"
+                                     hover
+                                     :items="items"
+                                     :fields="columns"
+                                     :per-page="perPage"
+                                     :current-page="currentPage">
+                                <template v-slot:cell(completed)="data">
+                                    <b-checkbox v-model="data.item.completed" :disabled="item.disabled" name="check-button" inline :key="index">
+                                    </b-checkbox>
+                                </template>
+                                <template v-slot:cell(description)="data">
+                                    {{ data.item.description }}
+                                </template>
+                                <template v-slot:cell(fullname)="data">
+                                    {{ data.item.patientname }} {{ data.item.patientlastname }}
+                                </template>
+                                <template v-slot:cell(personaldentist)="data">
+                                    Dr. Zobozdravnik
+                                </template>
+                                <template v-slot:cell(due_at)="data">
+                                    {{ data.item.due_at }}
+                                </template>
+                            </b-table>
+                        </b-col>
+                    </b-row>
+                </template>
+                <template v-slot:headerTitle>
+                    <h4 class="card-title">My Assignments</h4>
+                </template>
+                <template v-slot:body>
+                    <b-row>
+                        <b-col md="12" class="table-responsive table-borderless">
+                            <b-table id="my-assignments"
+                                     thead-class="d-none"
+                                     hover
+                                     :items="items"
+                                     :fields="columns"
+                                     :per-page="perPage"
+                                     :current-page="currentPage">
+                                <template v-slot:cell(completed)="data">
+                                    <b-checkbox v-model="data.item.completed" :disabled="item.disabled" name="check-button" inline :key="index">
+                                    </b-checkbox>
+                                </template>
+                                <template v-slot:cell(description)="data">
+                                    {{ data.item.description }}
+                                </template>
+                                <template v-slot:cell(fullname)="data">
+                                    {{ data.item.patientname }} {{ data.item.patientlastname }}
+                                </template>
+                                <template v-slot:cell(due_at)="data">
+                                    {{ data.item.due_at }}
+                                </template>
+                            </b-table>
+                        </b-col>
+                    </b-row>
+                </template>
+    </iq-card>
+    </b-col>
+    </b-row>
+    </b-container>
+</template> -->
 <style>
-  [dir=ltr] .iq-sidebar .iq-menu li.active {
-    background-color: #ffffff;
-    border-radius: 15px;
-}
+    [dir=ltr] .iq-sidebar .iq-menu li.active {
+        background-color: #ffffff;
+        border-radius: 15px;
+    }
 </style>
 <script>
 import { xray } from '../../config/pluginInit'
@@ -110,7 +197,7 @@ export default {
       index: [],
       items: rows,
       currentPage: 1,
-      perPage: 10,
+      perPage: 30,
       columns: [
         { label: 'Completed', key: 'completed', class: 'completed-column' },
         { label: 'Description', key: 'description', class: 'text-left' },
