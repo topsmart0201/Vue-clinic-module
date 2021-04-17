7de48e7e02f0e77a3e20467c4cf6f5279b8844a5
topsmart20 <oswellgrundmeier42@gmail.com>
2021-04-17 14:05:02 +0200

    Added backend method for fetching invoice by id

--- api/dao/daoInvoices.js
+++ api/dao/daoInvoices.js
@@ -18,6 +18,16 @@ const getInvoices = (request, response) => {
     })
 }
 
+const getInvoicesById = (request, response, id) => {
+    pool.query("SELECT prm_invoice.*,enquiries.email,enquiries.phone, countries.name as country FROM prm_invoice left join enquiries on prm_invoice.enquiries_id = enquiries.id left join countries on prm_invoice.enquiries_country_code = countries.code WHERE prm_invoice.invoice_id = $1", [id] , (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
 module.exports = {
-    getInvoices
+    getInvoices,
+    getInvoicesById
 }
--- api/server.js
+++ api/server.js
@@ -159,6 +159,14 @@ app.get('/api/invoices', (req, res) => {
         res.status(401).json("OK: user unauthorized")
 });
 
+app.get('/api/invoices/:id', (req, res) => {
+    const id = req.params.id
+    if (req.session.prm_user && req.session.prm_user.permissions && checkPermission(req.session.prm_user.permissions, invoicesPermission))
+        daoInvoices.getInvoicesById(req, res, id)
+    else
+        res.status(401).json("OK: user unauthorized")
+});
+
 ///////////////////////////////////
 // statistics & Reporting
 ///////////////////////////////////
