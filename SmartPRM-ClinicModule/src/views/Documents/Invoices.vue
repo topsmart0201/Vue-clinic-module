08cf4bb572f5c1e5ca24813b579238b1c13ee5c6
topsmart20 <oswellgrundmeier42@gmail.com>
2021-04-18 16:28:38 +0200

    Added filtering on invoices page

--- SmartPRM-ClinicModule/src/views/Documents/Invoices.vue
+++ SmartPRM-ClinicModule/src/views/Documents/Invoices.vue
@@ -11,19 +11,19 @@
                         <div class="iq-card-header-toolbar d-flex align-items-center" style="margin-top: -10px;">
                             <div class="iq-search-bar">
                                 <form action="#" class="searchbox">
-                                    <input type="text" v-on:keyup="myFunction($event.target.value)" class="text search-input" placeholder="Search" >
+                                    <input type="search" v-model="filter" class="text search-input" placeholder="Search">
                                     <a class="search-link" href="#"><i class="ri-search-line"></i></a>
                                 </form>
                             </div>
                             <iq-card>
-                                <b-form-group label-for="searchOptions"
-                                              label="Search By:">
-                                    <b-form-select plain
-                                                   v-model="selected"
-                                                   :options="searchOptions"
-                                                   id="searchFunction"
-                                                   @change="searchFunction($event)">
-                                    </b-form-select>
+                                <b-form-group label-for="searchOptions" label="Search By:">
+                                  <b-form-select
+                                    plain
+                                    v-model="filterOn"
+                                    :options="searchOptions"
+                                    id="searchFunction"
+                                  >
+                                  </b-form-select>
                                 </b-form-group>
                             </iq-card>
                         </div>
@@ -35,13 +35,23 @@
                   id="my-table"
                   bordered
                   hover
+                  :busy="!isDataLoaded"
                   @row-clicked="onInvoiceClick"
                   style="cursor: pointer;"
-                  :items="invoiceItems"
+                  :items="invoices"
                   :fields="columns"
                   :per-page="perPage"
                   :current-page="currentPage"
+                  :filter="filter"
+                  :filter-included-fields="filterOn"
+                  @filtered="onFiltered"
                 >
+                <template #table-busy>
+                  <div class="text-center text-primary my-2">
+                    <b-spinner class="align-middle"></b-spinner>
+                    <strong class="loading">Loading...</strong>
+                  </div>
+                </template>
                 </b-table>
               </b-col>
             </b-row>
@@ -49,10 +59,10 @@
               <b-collapse id="collapse-6" class="mb-2"> </b-collapse>
               <div class="mt-3">
                 <b-pagination
-                v-model="currentPage"
-                :total-rows="invoiceItems.length"
-                :per-page="perPage"
-                aria-controls="my-table"
+                  v-model="currentPage"
+                  :total-rows="totalRows"
+                  :per-page="perPage"
+                  aria-controls="my-table"
               ></b-pagination>
               </div>
             </template>
@@ -76,83 +86,77 @@ export default {
       dropDownText: '',
       selected: this.value,
       searchOptions: [
-        { value: 'invoice_no', text: 'Number' },
-        { value: 'patient_name', text: 'Patient Name' },
-        { value: 'date', text: 'Date' },
-        { value: 'issued_by', text: 'Issued by' },
-        { value: 'amount', text: 'Invoice amount' },
-        { value: 'status', text: 'Status' }
+        { value: ['invoice_number'], text: 'Number' },
+        { value: ['patient_name'], text: 'Patient Name' },
+        { value: ['invoice_time'], text: 'Date' },
+        { value: ['company_name'], text: 'Issued by' },
+        { value: ['total_with_vat'], text: 'Invoice amount' },
+        { value: ['status'], text: 'Status' }
       ],
-      invoiceItems: [],
+      invoices: [],
       currentPage: 1,
       perPage: 10,
       columns: [
         { label: this.$t('invoices.invoicesColumn.no'), key: 'invoice_number', class: 'text-left' },
         { label: this.$t('invoices.invoicesColumn.patientName'),
           key: 'patient_name',
+          class: 'text-left',
           formatter: (value, key, item) => {
             return item.enquiries_name + ' ' + item.enquiries_last_name
           },
-          class: 'text-left' },
+          filterByFormatted: true
+        },
         { label: this.$t('invoices.invoicesColumn.date'),
           key: 'invoice_time',
           class: 'text-left',
           formatter: value => {
             return value.split('T').shift()
-          }
+          },
+          filterByFormatted: true
         },
         { label: this.$t('invoices.invoicesColumn.issuedBy'), key: 'company_name', class: 'text-left' },
         { label: this.$t('invoices.invoicesColumn.amount'), key: 'total_with_vat', class: 'text-left' },
         { label: this.$t('invoices.invoicesColumn.status'),
           key: 'status',
+          class: 'text-left',
           formatter: (value, key, item) => {
             if (item.paid_amount === '$0.00') {
               return 'Unpaid'
             }
             return item.total_with_vat === item.paid_amount ? 'Paid' : 'Partialy Paid'
           },
-          class: 'text-left' }
-      ]
+          filterByFormatted: true
+        }
+      ],
+      filter: '',
+      filterOn: [],
+      isDataLoaded: false,
+      totalRows: 1
     }
   },
   methods: {
-    default () {
-      return {
-        invoice_no: this.rows.length,
-        patient_name: '',
-        amount: '',
-        issued_by: '',
-        status: '',
-        date: ''
-      }
-    },
     onInvoiceClick (item) {
       this.$router.push({ path: `/documents/invoices/${item.invoice_id}` })
-    },
-    searchFunction (event) {
-      this.dropDownText = event
-      console.log('SEARCHBY OPTION:', event)
-      return event
-    },
-    myFunction (event) {
-      console.log('evemt', event)
-      console.log('this.dropDownText', this.dropDownText)
-
-      if (this.dropDownText) {
-        var sorted = this.invoiceItems.filter((item) => {
-          return item[this.dropDownText].toLowerCase().includes(event.toLowerCase())
-        })
-        this.paginatedItems = sorted
-      }
-      console.log('sorted', sorted)
     },
     add_invoice () {
       this.$router.push('/extra-pages/new-invoice')
     },
     getInvoices () {
       getInvoices().then(response => {
-        this.invoiceItems = response
+        this.invoices = response
+        this.setTotalRows(this.invoices.length)
+        this.toggleDataLoaded()
       })
+    },
+    onFiltered (filteredItems) {
+      this.totalRows = filteredItems.length
+      this.currentPage = 1
+    },
+    setTotalRows (number) {
+      this.totalRows = number
+    },
+    toggleDataLoaded () {
+      this.isDataLoaded = !this.isDataLoaded
     }
   },
   mounted () {
