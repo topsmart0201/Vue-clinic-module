a17feecc1cb7b0e9e238980a7601e42bb8bebcf5
topsmart20 <oswellgrundmeier42@gmail.com>
2021-04-18 22:15:21 +0200

    Remove all unused files and update routing

--- SmartPRM-ClinicModule/src/router/index.js
+++ SmartPRM-ClinicModule/src/router/index.js
@@ -2,104 +2,42 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 /* Layouts */
 import Layout1 from '../layouts/Layout1'
-import AuthLayout1 from '../layouts/AuthLayouts/AuthLayout1'
-/* Dashboards View */
-import Dashboard1 from '../views/Dashboards/Dashboard1.vue'
-import Dashboard3 from '../views/Dashboards/Dashboard3'
-import Dashboard4 from '../views/Dashboards/Dashboard4'
-/* UIElements View */
-import UiAlerts from '../views/core/UiAlerts.vue'
-import UiButtons from '../views/core/UiButtons.vue'
-import UiBadges from '../views/core/UiBadges.vue'
-import UiBreadcrumb from '../views/core/UiBreadcrumb.vue'
-import UiColors from '../views/core/UiColors.vue'
-import UiTypography from '../views/core/UiTypography.vue'
-import UiCards from '../views/core/UiCards.vue'
-import UiCarousel from '../views/core/UiCarousel.vue'
-import UiEmbedVideo from '../views/core/UiEmbedVideo.vue'
-import UiGrid from '../views/core/UiGrid.vue'
-import UiModal from '../views/core/UiModal.vue'
-import UiListGroup from '../views/core/UiListGroup.vue'
-import UiImages from '../views/core/UiImages.vue'
-import UiMediaObject from '../views/core/UiMediaObject.vue'
-import UiTooltips from '../views/core/UiTooltips.vue'
-import UiPopovers from '../views/core/UiPopovers.vue'
-import UiNotifications from '../views/core/UiNotifications.vue'
-import UiTabs from '../views/core/UiTabs.vue'
-import UiPagination from '../views/core/UiPagination.vue'
-import UiProgressBars from '../views/core/UiProgressBars.vue'
-/* Authentic View */
-import SignIn1 from '../views/AuthPages/Default/SignIn1'
-import SignUp1 from '../views/AuthPages/Default/SignUp1'
-import RecoverPassword1 from '../views/AuthPages/Default/RecoverPassword1'
-import LockScreen1 from '../views/AuthPages/Default/LockScreen1'
-import ConfirmMail1 from '../views/AuthPages/Default/ConfirmMail1'
-/* Extra Pages */
-import Invoice from '../views/Pages/Invoice'
-import OfferExample from '../views/Pages/OfferExample'
-import AdvancePaymentExample from '../views/Pages/AdvancePaymentExample'
-import NewInvoice from '../views/Pages/NewInvoice'
-/* Patients */
-import ViewPatient from '../views/User/ViewPatient'
-import Patients from '../views/User/Patients'
-/* Apps Views */
-import EmailListing from '../views/Apps/Email/EmailListing'
-import EmailCompose from '../views/Apps/Email/EmailCompose'
+/* Calendar */
 import Calendar from '../views/Apps/Calendar/Calendar'
-import ChatIndex from '../views/Apps/Chat/Index'
-/* Icon Views */
-import IconDripicons from '../views/Icons/IconDripicons'
-import IconFontawesome5 from '../views/Icons/IconFontawesome5'
-import IconLineAwesome from '../views/Icons/IconLineAwesome'
-import IconRemixicon from '../views/Icons/IconRemixicon'
-import IconUnicons from '../views/Icons/IconUnicons'
-/* Tables Views */
-import TablesBasic from '../views/Tables/TablesBasic'
-import DataTable from '../views/Tables/DataTable'
-import EditableTable from '../views/Tables/EditableTable'
-import ApexCharts from '../views/Charts/ApexCharts'
-import AmCharts from '../views/Charts/AmCharts'
-import MorrisCharts from '../views/Charts/MorrisCharts'
-import HighCharts from '../views/Charts/HighCharts'
-/* Form View */
-import FormLayout from '../views/Forms/FormLayout'
-import FormValidates from '../views/Forms/FormValidates'
-import FormSwitches from '../views/Forms/FormSwitches'
-import FormRadios from '../views/Forms/FormRadios'
-import FormCheckboxes from '../views/Forms/FormCheckboxes'
-/* User View */
-import Profile from '../views/User/Profile'
-import ProfileEdit from '../views/User/ProfileEdit'
-import AddUser from '../views/User/AddUser'
-/* Todo */
-import Callback from '../views/AuthPages/Default/Callback'
-/* Plugins Views */
-import DatepickerDemo from '../views/Plugins/DatepickerDemo'
-/* Documents */
-import Offers from '../views/Documents/Offers'
-import Invoices from '../views/Documents/Invoices'
-import AdvInvoices from '../views/Documents/AdvPayments'
 /* Assignments */
 import Assignments from '../views/Assignments/Assignments'
+import Dashboard4 from '../views/Dashboards/Dashboard4'
+import AdvInvoices from '../views/Documents/AdvPayments'
+import Invoice from '../views/Documents/Invoice'
+import Invoices from '../views/Documents/Invoices'
+import NewInvoice from '../views/Documents/NewInvoice'
+import Offers from '../views/Documents/Offers'
+/* Documents */
+import Patients from '../views/Patients/Patients'
+/* Patients */
+import ViewPatient from '../views/Patients/ViewPatient'
+/* Extra Pages */
+import Doctor from '../views/Reporting/Doctor'
 /* Reporting */
 import Emazing from '../views/Reporting/Emazing'
-import Doctor from '../views/Reporting/Doctor'
 import TaxAuthority from '../views/Reporting/TaxAuthority'
-/* Settings */
-import Users from '../views/Settings/Users'
+import Invoicing from '../views/Settings/Invoicing'
 import Locations from '../views/Settings/Locations'
 import ServicesAndProducts from '../views/Settings/ServicesAndProducts'
-import Invoicing from '../views/Settings/Invoicing'
+/* Settings */
+import Users from '../views/Settings/Users'
 /* Statistics */
-import PersonalStatistics from '../views/Statistics/PersonalStatistics.vue'
+import ClinicStatistics from '../views/Statistics/ClinicStatistics'
+import PersonalStatistics from '../views/Statistics/PersonalStatistics'
+
 Vue.use(VueRouter)
 
 const childRoutes = (prop, mode) => [
   {
-    path: 'home-1',
-    name: prop + '.home-1',
-    meta: { dark: mode, auth: true, name: 'Home 1' },
-    component: Dashboard1
+    path: '/',
+    name: prop + '.home',
+    meta: { dark: mode, auth: true, name: 'Home' },
+    component: Dashboard4
   },
   {
     path: 'calendar',
@@ -118,30 +56,6 @@ const childRoutes = (prop, mode) => [
     name: prop + '.assignments',
     meta: { dark: mode, auth: true, name: 'assignments' },
     component: Assignments
-  },
-  // {
-  //   path: 'home-2',
-  //   name: prop + '.home-2',
-  //   meta: { dark: mode, auth: true, name: 'Home 2' },
-  //   component: Dashboard2
-  // },
-  {
-    path: 'home-3',
-    name: prop + '.home-3',
-    meta: { dark: mode, auth: true, name: 'Home 3' },
-    component: Dashboard3
-  },
-  {
-    path: '/',
-    name: prop + '.home',
-    meta: { dark: mode, auth: true, name: 'Home' },
-    component: Dashboard4
-  },
-  {
-    path: '/home',
-    name: prop + '.home',
-    meta: { dark: mode, auth: true, name: 'Home' },
-    component: Dashboard4
   }
 ]
 const documentChildRoute = (prop, mode) => [
@@ -179,187 +93,12 @@ const patientChildRoute = (prop, mode) => [
   }
 ]
 
-const coreChildRoute = (prop, mode) => [
-  {
-    path: 'ui-colors',
-    name: prop + '.color',
-    meta: { dark: mode, auth: true, name: 'Colors' },
-    component: UiColors
-  },
-  {
-    path: 'ui-typography',
-    name: prop + '.typography',
-    meta: { dark: mode, auth: true, name: 'Typography' },
-    component: UiTypography
-  },
-  {
-    path: 'ui-alerts',
-    name: prop + '.alert',
-    meta: { dark: mode, auth: true, name: 'Alert' },
-    component: UiAlerts
-  },
-  {
-    path: 'ui-buttons',
-    name: prop + '.button',
-    meta: { dark: mode, auth: true, name: 'Button' },
-    component: UiButtons
-  },
-  {
-    path: 'ui-breadcrumb',
-    name: prop + '.breadcrumb',
-    meta: { dark: mode, auth: true, name: 'Breadcrumb' },
-    component: UiBreadcrumb
-  },
-  {
-    path: 'ui-badges',
-    name: prop + '.badges',
-    meta: { dark: mode, auth: true, name: 'Badges' },
-    component: UiBadges
-  },
-  {
-    path: 'ui-cards',
-    name: prop + '.cards',
-    meta: { dark: mode, auth: true, name: 'Card' },
-    component: UiCards
-  },
-  {
-    path: 'ui-carousel',
-    name: prop + '.carousel',
-    meta: { dark: mode, auth: true, name: 'Carousel' },
-    component: UiCarousel
-  },
-  {
-    path: 'ui-grid',
-    name: prop + '.grid',
-    meta: { dark: mode, auth: true, name: 'Grid' },
-    component: UiGrid
-  },
-  {
-    path: 'ui-embed-video',
-    name: prop + '.embed-video',
-    meta: { dark: mode, auth: true, name: 'Embed Video' },
-    component: UiEmbedVideo
-  },
-  {
-    path: 'ui-modal',
-    name: prop + '.modal',
-    meta: { dark: mode, auth: true, name: 'Model' },
-    component: UiModal
-  },
-  {
-    path: 'ui-listgroup',
-    name: prop + '.listgroup',
-    meta: { dark: mode, auth: true, name: 'List Group' },
-    component: UiListGroup
-  },
-  {
-    path: 'ui-images',
-    name: prop + '.images',
-    meta: { dark: mode, auth: true, name: 'Image' },
-    component: UiImages
-  },
-  {
-    path: 'ui-media-object',
-    name: prop + '.media-object',
-    meta: { dark: mode, auth: true, name: 'Media Object' },
-    component: UiMediaObject
-  },
-  {
-    path: 'ui-pagination',
-    name: prop + '.pagination',
-    meta: { dark: mode, auth: true, name: 'Paginations' },
-    component: UiPagination
-  },
-  {
-    path: 'ui-progressbars',
-    name: prop + '.progressbars',
-    meta: { dark: mode, auth: true, name: 'Progressbar' },
-    component: UiProgressBars
-  },
-  {
-    path: 'ui-tooltips',
-    name: prop + '.tooltips',
-    meta: { dark: mode, auth: true, name: 'Tooltip' },
-    component: UiTooltips
-  },
-  {
-    path: 'ui-popovers',
-    name: prop + '.popovers',
-    meta: { dark: mode, auth: true, name: 'Popover' },
-    component: UiPopovers
-  },
-  {
-    path: 'ui-notifications',
-    name: prop + '.notifications',
-    meta: { dark: mode, auth: true, name: 'Notification' },
-    component: UiNotifications
-  },
-  {
-    path: 'ui-tabs',
-    name: prop + '.tabs',
-    meta: { dark: mode, auth: true, name: 'Tabs' },
-    component: UiTabs
-  }
-]
-const formChildRoute = (prop, mode = false) => [
-  {
-    path: 'form-layout',
-    name: prop + '.layout',
-    meta: { dark: mode, auth: true, name: 'Layout' },
-    component: FormLayout
-  },
-  {
-    path: 'form-validate',
-    name: prop + '.validate',
-    meta: { dark: mode, auth: true, name: 'Validate' },
-    component: FormValidates
-  },
-  {
-    path: 'form-switches',
-    name: prop + '.switch',
-    meta: { dark: mode, auth: true, name: 'Switch' },
-    component: FormSwitches
-  },
-  {
-    path: 'form-radios',
-    name: prop + '.radio',
-    meta: { dark: mode, auth: true, name: 'Radio' },
-    component: FormRadios
-  },
-  {
-    path: 'form-checkboxes',
-    name: prop + '.checkbox',
-    meta: { dark: mode, auth: true, name: 'Checkbox' },
-    component: FormCheckboxes
-  }
-]
-const tableChildRoute = (prop, mode = false) => [
-  {
-    path: 'tables-basic',
-    name: prop + '.basic',
-    meta: { dark: mode, auth: true, name: 'Basic' },
-    component: TablesBasic
-  },
-  {
-    path: 'data-table',
-    name: prop + '.dataTable',
-    meta: { dark: mode, auth: true, name: 'Datatable' },
-    component: DataTable
-  },
-  {
-    path: 'editable',
-    name: prop + '.editable',
-    meta: { dark: mode, auth: true, name: 'Editable' },
-    component: EditableTable
-  }
-]
-
 const statisticsChildRoute = (prop, mode = false) => [
   {
     path: 'statistics-for-clinic',
     name: prop + '.statistics-for-clinic',
     meta: { dark: mode, auth: true, name: 'Statistics for Clinic' },
-    component: Dashboard1
+    component: ClinicStatistics
   },
   {
     path: 'personal-statistics',
@@ -417,180 +156,12 @@ const settingsChildRoute = (prop, mode = false) => [
   }
 ]
 
-const iconChildRoute = (prop, mode = false) => [
-  {
-    path: 'dripicons',
-    name: prop + '.dripicons',
-    meta: { dark: mode, auth: true, name: 'Dripicons' },
-    component: IconDripicons
-  },
-  {
-    path: 'fontawesome-5',
-    name: prop + '.fontawesome-5',
-    meta: { dark: mode, auth: true, name: 'Font Awsome' },
-    component: IconFontawesome5
-  },
-  {
-    path: 'lineawesome',
-    name: prop + '.lineawesome',
-    meta: { dark: mode, auth: true, name: 'Line Awsome' },
-    component: IconLineAwesome
-  },
-  {
-    path: 'remixicon',
-    name: prop + '.remixicon',
-    meta: { dark: mode, auth: true, name: 'Remixicon' },
-    component: IconRemixicon
-  },
-  {
-    path: 'unicons',
-    name: prop + '.unicons',
-    meta: { dark: mode, auth: true, name: 'Unicon' },
-    component: IconUnicons
-  }
-]
-
-const chartChildRoutes = (prop, mode = false) => [
-  {
-    path: 'apex-charts',
-    name: prop + '.apex',
-    meta: { dark: mode, auth: true, name: 'Apex Chat' },
-    component: ApexCharts
-  },
-  {
-    path: 'am-charts',
-    name: prop + '.am',
-    meta: { dark: mode, auth: true, name: 'Am Chart' },
-    component: AmCharts
-  },
-  {
-    path: 'high-charts',
-    name: prop + '.high',
-    meta: { dark: mode, auth: true, name: 'High Chart' },
-    component: HighCharts
-  },
-  {
-    path: 'morris-charts',
-    name: prop + '.morris',
-    meta: { dark: mode, auth: true, name: 'Mirris Chart' },
-    component: MorrisCharts
-  }
-]
-
-const appChildRoute = (prop, mode = false) => [
-  // {
-  //   path: 'calendar',
-  //   name: prop + '.calendar',
-  //   meta: { dark: mode, auth: true, name: 'Calendar' },
-  //   component: Calendar
-  // },
-  {
-    path: 'chat',
-    name: prop + '.chat',
-    meta: { dark: mode, auth: true, name: 'Chat' },
-    component: ChatIndex
-  },
-  {
-    path: 'email',
-    name: prop + '.email',
-    meta: { dark: mode, auth: true, name: 'Email' },
-    component: EmailListing
-  },
-  {
-    path: 'email-compose',
-    name: prop + '.email.compose',
-    meta: { dark: mode, auth: true, name: 'New Email' },
-    component: EmailCompose
-  }
-]
-
-const authChildRoutes = (prop, mode = false) => [
-  {
-    path: 'sign-in1',
-    name: prop + '.sign-in1',
-    meta: { dark: mode, auth: true },
-    component: SignIn1
-  },
-  {
-    path: 'sign-up1',
-    name: prop + '.sign-up1',
-    meta: { dark: mode, auth: true },
-    component: SignUp1
-  },
-  {
-    path: 'password-reset1',
-    name: prop + '.password-reset1',
-    meta: { dark: mode, auth: true },
-    component: RecoverPassword1
-  },
-  {
-    path: 'lock-screen1',
-    name: prop + '.lock-screen1',
-    meta: { dark: mode, auth: true },
-    component: LockScreen1
-  },
-  {
-    path: 'confirm-mail1',
-    name: prop + '.confirm-mail1',
-    meta: { dark: mode, auth: true },
-    component: ConfirmMail1
-  }
-]
-
 const defaultlayout = (prop, mode = false) => [
-  {
-    path: 'offer-example',
-    name: prop + '.offer-example',
-    meta: { dark: mode, auth: true, name: 'Offer Example' },
-    component: OfferExample
-  },
-  {
-    path: 'advance-payment-example',
-    name: prop + '.advance-payment-example',
-    meta: { dark: mode, auth: true, name: 'Advance Payment Example' },
-    component: AdvancePaymentExample
-  },
   {
     path: 'new-invoice',
     name: prop + '.new-invoice',
     meta: { dark: mode, auth: true, name: 'New Invoices' },
     component: NewInvoice
-  }
-]
-
-const userChildRoute = (prop, mode = false) => [
-  {
-    path: 'profile',
-    name: prop + '.profile',
-    meta: { dark: mode, auth: true, name: 'Profile' },
-    component: Profile
-  },
-  {
-    path: 'profile-edit',
-    name: prop + '.edit',
-    meta: { dark: mode, auth: true, name: 'Edit Profile' },
-    component: ProfileEdit
-  },
-  {
-    path: 'add-user',
-    name: prop + '.add',
-    meta: { dark: mode, auth: true, name: 'Add Profile' },
-    component: AddUser
-  }
-  // {
-  //   path: 'user-list',
-  //   name: prop + '.list',
-  //   meta: { dark: mode, auth: true, name: 'User List' },
-  //   component: UserList
-  // }
-]
-
-const pluginsChildRoute = (prop, mode = false) => [
-  {
-    path: 'datepicker',
-    name: prop + '.datepicker',
-    meta: { dark: mode, auth: true, name: 'Datepicker' },
-    component: DatepickerDemo
   }
 ]
 
@@ -601,20 +172,6 @@ const routes = [
     component: Layout1,
     meta: { auth: true },
     children: childRoutes('dashboard')
-  },
-  // {
-  //   path: '/assignment',
-  //   name: 'dashboard3',
-  //   component: Layout1,
-  //   meta: { auth: true },
-  //   children: childRoutes('dashboard3')
-  // },
-  {
-    path: '/core',
-    name: 'core',
-    component: Layout1,
-    meta: { auth: true },
-    children: coreChildRoute('core')
   },
   {
     path: '/documents',
@@ -630,13 +187,6 @@ const routes = [
     meta: { auth: true },
     children: patientChildRoute('patients')
   },
-  // {
-  //   path: '/invoices',
-  //   name: 'invoices',
-  //   component: Layout1,
-  //   meta: { auth: true },
-  //   children: invoiceChildRoute('invoices')
-  // },
   {
     path: '/statistics',
     name: 'statistics',
@@ -659,73 +209,11 @@ const routes = [
     children: settingsChildRoute('settings')
   },
   {
-    path: '/table',
-    name: 'table',
-    component: Layout1,
-    meta: { auth: true },
-    children: tableChildRoute('table')
-  },
-  {
-    path: '/chart',
-    name: 'chart',
-    component: Layout1,
-    meta: { auth: true },
-    children: chartChildRoutes('chart')
-  },
-  {
-    path: '/icon',
-    name: 'icon',
-    component: Layout1,
-    meta: { auth: true },
-    children: iconChildRoute('icon')
-  },
-  {
-    path: '/auth',
-    name: 'auth1',
-    component: AuthLayout1,
-    meta: { auth: true },
-    children: authChildRoutes('auth1')
-  },
-  {
     path: '/extra-pages',
     name: 'extra-pages',
     component: Layout1,
     meta: { auth: true },
     children: defaultlayout('extra-pages')
-  },
-  {
-    path: '/app',
-    name: 'app',
-    component: Layout1,
-    meta: { auth: true },
-    children: appChildRoute('app')
-  },
-  {
-    path: '/doctor',
-    name: 'doctor',
-    component: Layout1,
-    meta: { auth: true },
-    children: userChildRoute('doctor')
-  },
-  {
-    path: '/form',
-    name: 'form',
-    component: Layout1,
-    meta: { auth: true },
-    children: formChildRoute('form')
-  },
-  {
-    path: '/plugins',
-    name: 'plugins',
-    component: Layout1,
-    meta: { auth: true },
-    children: pluginsChildRoute('plugins')
-  },
-  {
-    path: '/callback',
-    name: 'callback',
-    meta: { auth: false },
-    component: Callback
   }
 ]
 
---
+++ SmartPRM-ClinicModule/src/views/Statistics/ClinicStatistics.vue
@@ -0,0 +1,221 @@
+<template>
+  <b-container fluid>
+    <b-row>
+      <b-col lg="12">
+        <b-row>
+          <b-col md="6" lg="3">
+            <iq-card class-name="iq-card-block iq-card-stretch iq-card-height" body-class="iq-bg-primary rounded">
+              <template v-slot:body>
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="rounded-circle iq-card-icon bg-primary"><i class="ri-user-fill"></i></div>
+                  <div class="text-right">
+                    <h2 class="mb-0"><span class="counter">{{ attendance }}</span></h2>
+                    <h5 class="">{{ $t('statisticsForClinic.attendance') }}</h5>
+                  </div>
+                </div>
+              </template>
+            </iq-card>
+          </b-col>
+          <b-col md="6" lg="3">
+            <iq-card class-name="iq-card-block iq-card-stretch iq-card-height" body-class="iq-bg-warning rounded">
+              <template v-slot:body >
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="rounded-circle iq-card-icon bg-warning"><i class="ri-women-fill"></i></div>
+                  <div class="text-right">
+                    <h2 class="mb-0"><span class="counter">3450</span></h2>
+                    <h5 class="">{{ $t('statisticsForClinic.nurses') }}</h5>
+                  </div>
+                </div>
+              </template>
+            </iq-card>
+          </b-col>
+          <b-col md="6" lg="3">
+            <iq-card class-name="iq-card-block iq-card-stretch iq-card-height" body-class="iq-bg-danger rounded">
+              <template v-slot:body >
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="rounded-circle iq-card-icon bg-danger"><i class="ri-group-fill"></i></div>
+                  <div class="text-right">
+                    <h2 class="mb-0"><span class="counter">3500</span></h2>
+                    <h5 class="">{{ $t('statisticsForClinic.patients') }}</h5>
+                  </div>
+                </div>
+              </template>
+            </iq-card>
+          </b-col>
+          <b-col md="6" lg="3">
+            <iq-card class-name="iq-card-block iq-card-stretch iq-card-height" body-class="iq-bg-info rounded">
+              <template v-slot:body >
+                <div class="d-flex align-items-center justify-content-between">
+                  <div class="rounded-circle iq-card-icon bg-info"><i class="ri-hospital-line"></i></div>
+                  <div class="text-right">
+                    <h2 class="mb-0"><span class="counter">4500</span></h2>
+                    <h5 class="">{{ $t('statisticsForClinic.pharmacists') }}</h5>
+                  </div>
+                </div>
+              </template>
+            </iq-card>
+          </b-col>
+        </b-row>
+      </b-col>
+      <b-col sm="12">
+        <iq-card class-name="iq-card-block iq-card-stretch iq-card-height">
+          <template v-slot:headerTitle>
+              <h4 class="card-title">{{ $t('statisticsForClinic.statisticsForClinicHeader') }}</h4>
+          </template>
+          <template v-slot:body>
+            <div class="iq-card-body pb-0 mt-3">
+              <div class="row text-center">
+                <div class="col-sm-3 col-6">
+                  <h4 class="margin-0">€ 305 </h4>
+                  <p class="text-muted">{{ $t('statisticsForClinic.todaysIncome') }}</p>
+                </div>
+                <div class="col-sm-3 col-6">
+                  <h4 class="margin-0">€ 999 </h4>
+                  <p class="text-muted">{{ $t('statisticsForClinic.weeksIncome') }}</p>
+                </div>
+                <div class="col-sm-3 col-6">
+                  <h4 class="margin-0">€ 4999 </h4>
+                  <p class="text-muted">{{ $t('statisticsForClinic.monthsIncome') }}</p>
+                </div>
+                <div class="col-sm-3 col-6">
+                  <h4 class="margin-0">€ 90.000 </h4>
+                  <p class="text-muted">{{ $t('statisticsForClinic.yearsIncome') }}</p>
+                </div>
+              </div>
+            </div>
+            <div id="home-servey-chart"></div>
+            <!-- <ApexChart element="home-chart-09" :chartOption="homesurvey" v-if="$route.meta.dark"/> -->
+            <!-- <ApexChart element="home-chart-09" :chartOption="chart9" v-else/> -->
+            <ApexChart element="home-chart-09" :chartOption="chart9"/>
+          </template>
+        </iq-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+<script>
+import { xray } from '../../config/pluginInit'
+import IqCard from '../../components/xray/cards/iq-card'
+import { clinicStatistics, clinicStatisticsAttendance } from '../../services/statistics'
+let dates = []
+let totalRevenue = []
+export default {
+  name: 'Dashboard1',
+  components: { IqCard },
+  mounted () {
+    xray.index()
+    this.getStatistics()
+    this.getAttendance()
+  },
+  data () {
+    return {
+      attendance: 0,
+      statistics: [],
+      slickOptions: {
+        centerMode: false,
+        centerPadding: '60px',
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        responsive: [{
+          breakpoint: 992,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '30',
+            slidesToShow: 3
+          }
+        }, {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '15',
+            slidesToShow: 1
+          }
+        }]
+      },
+      chart9: {
+        series: [{
+          name: 'Total Revenue',
+          data: totalRevenue
+        }],
+        chart: {
+          type: 'bar',
+          height: 350
+        },
+        plotOptions: {
+          bar: {
+            colors: {
+              ranges: [{
+                from: -100,
+                to: -46,
+                color: '#e64141'
+              }, {
+                from: -45,
+                to: 0,
+                color: '#089bab'
+              }, {
+                from: 0,
+                to: 20,
+                color: '#FC9F5B'
+              }]
+            },
+            columnWidth: '80%'
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        yaxis: {
+          title: {
+            text: 'Growth'
+          }
+          // labels: {
+          //   formatter: function (y) {
+          //     return y.toFixed(0) + '%'
+          //   }
+          // }
+        },
+        xaxis: {
+          type: 'datetime',
+          categories: dates,
+          labels: {
+            rotate: -90
+          }
+        }
+      }
+    }
+  },
+  methods: {
+    getStatistics () {
+      clinicStatistics().then(response => {
+        if (typeof response !== 'string') {
+          this.statistics = response
+          this.statistics.forEach(function (item) {
+            dates.push(item.date.split('T').shift())
+            totalRevenue.push(parseFloat(item.totalrevenue))
+          })
+        } else {
+          console.error(response)
+        }
+      })
+    },
+    getAttendance () {
+      clinicStatisticsAttendance().then(response => {
+        if (typeof response !== 'string') {
+          this.attendance = response[0].count
+        } else {
+          console.error(response)
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.iq-card-body{
+  flex: unset;
+}
+</style>
