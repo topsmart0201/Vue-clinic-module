5c981f044ae2fc83ad8feeb6c1b7b06de8ecf9f5
topsmart20 <oswellgrundmeier42@gmail.com>
2021-04-19 10:38:05 +0200

    Update invoices and invoice pages, and invoice backend method

--- api/dao/daoInvoices.js
+++ api/dao/daoInvoices.js
@@ -19,7 +19,7 @@ const getInvoices = (request, response) => {
 }
 
 const getInvoicesById = (request, response, id) => {
-    pool.query("SELECT prm_invoice.*,enquiries.email,enquiries.phone, countries.name as country FROM prm_invoice left join enquiries on prm_invoice.enquiries_id = enquiries.id left join countries on prm_invoice.enquiries_country_code = countries.code WHERE prm_invoice.invoice_id = $1", [id] , (error, results) => {
+    pool.query("SELECT prm_invoice.*,enquiries.email,enquiries.phone, countries.name as country FROM prm_invoice left join enquiries on prm_invoice.enquiries_id = enquiries.id left join countries on prm_invoice.enquiries_country_code = countries.code WHERE prm_invoice.invoice_number = $1", [id] , (error, results) => {
         if (error) {
             throw error
         }
