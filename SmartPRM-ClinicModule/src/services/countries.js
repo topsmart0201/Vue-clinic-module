ca0cc36a8a2262cdad90c0af391a2289a31111cf
topsmart20 <oswellgrundmeier42@gmail.com>
2021-04-19 23:00:42 +0200

    Added service for countries

---
+++ SmartPRM-ClinicModule/src/services/countries.js
@@ -0,0 +1,11 @@
+export async function getCountries () {
+  const rawResponse = await fetch('/api/countries', {
+    method: 'GET',
+    credentials: 'same-origin',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+  return rawResponse.json()
+}
