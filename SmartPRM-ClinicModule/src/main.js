229a19f18949a74a71b327d0eab05730e27ce893
topsmart20 <oswellgrundmeier42@gmail.com>
2021-04-23 12:03:55 +0200

    Remove console errors

--- SmartPRM-ClinicModule/src/main.js
+++ SmartPRM-ClinicModule/src/main.js
@@ -9,7 +9,7 @@ import Raphael from 'raphael/raphael'
 import './plugins'
 import './registerServiceWorker'
 import i18n from './i18n'
-import Rollbar from 'rollbar';
+import Rollbar from 'rollbar'
 
 global.Raphael = Raphael
 Vue.config.productionTip = false
@@ -17,17 +17,17 @@ Vue.config.productionTip = false
 Vue.prototype.$rollbar = new Rollbar({
   accessToken: '2e6ee55565034b49987652bbe5ed3060',
   captureUncaught: true,
-  captureUnhandledRejections: true,
-});
+  captureUnhandledRejections: true
+})
 
 // If you have already set up a global error handler,
 // just add `vm.$rollbar.error(err)` to the top of it.
 // If not, this simple example will preserve the app’s existing
 // behavior while also reporting uncaught errors to Rollbar.
 Vue.config.errorHandler = (err, vm, info) => {
-  vm.$rollbar.error(err);
-  throw err; // rethrow
-};
+  vm.$rollbar.error(err)
+  throw err // rethrow
+}
 
 let vm = new Vue({
   router,
