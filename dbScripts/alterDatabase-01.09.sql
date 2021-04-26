17c92fb1e3398d51ed2a4536f49084867044ac24
topsmart20 <oswellgrundmeier42@gmail.com>
2021-04-26 11:36:07 +0200

    minor modification to the database

--- dbScripts/alterDatabase-01.09.sql
+++ dbScripts/alterDatabase-01.09.sql
@@ -3,7 +3,7 @@
 --############################################################
 
 ALTER TABLE enquiries
-ADD COLUMN insurance_no				INT,
+ADD COLUMN insurance_no				VARCHAR(64),
 ADD COLUMN insured_at				VARCHAR(128);
 
 UPDATE db_version SET version ='01.09', version_date=CURRENT_DATE WHERE resource='Tables';
