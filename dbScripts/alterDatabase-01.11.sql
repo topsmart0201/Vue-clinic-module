4c3884b14c23f61a40a86f709fa03c59698744d6
topsmart20 <oswellgrundmeier42@gmail.com>
2021-04-28 14:32:25 +0200

    further db alterations

--- dbScripts/alterDatabase-01.11.sql
+++ dbScripts/alterDatabase-01.11.sql
@@ -10,6 +10,11 @@ ALTER TABLE prm_invoice_item DROP COLUMN product_id;
 ALTER TABLE prm_invoice_item ADD COLUMN product_id SERIAL
 CONSTRAINT prm_invoice_item_prm_product_fk REFERENCES prm_product (product_id);
 
+ALTER TABLE prm_company_premise DROP COLUMN company_id;
+
+ALTER TABLE prm_company_premise ADD COLUMN company_id INT
+CONSTRAINT prm_company_premise_prm_company_fk REFERENCES prm_company (company_id);
+
 UPDATE db_version SET version ='01.11', version_date=CURRENT_DATE WHERE resource='Tables';
 
 
