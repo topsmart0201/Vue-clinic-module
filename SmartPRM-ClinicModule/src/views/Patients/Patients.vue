08be71a66beb6d65597b9ca5ec94ed4f4b9f89c0
topsmart20 <oswellgrundmeier42@gmail.com>
2021-05-06 15:39:33 +0200

    Fix mobile look of search arean of patients page

--- SmartPRM-ClinicModule/src/views/Patients/Patients.vue
+++ SmartPRM-ClinicModule/src/views/Patients/Patients.vue
@@ -5,17 +5,17 @@
                 <iq-card>
                     <template v-slot:headerTitle>
                         <h3 class="card-title" style="margin-top: 10px;">{{ $t('patients.patientsHeader') }}</h3>
-                        <div class="btn-add-patient">
+                        <div class="btn-add-patient col-12 col-sm-3 col-md-2 mb-4 mb-sm-0 aki">
                             <b-button variant="primary" @click="add"><i class="ri-add-line mr-2"></i>{{ $t('patients.addPatient') }}</b-button>
                         </div>
-                        <div class="iq-card-header-toolbar d-flex align-items-center" style="margin-top: -10px;">
+                        <div class="iq-card-header-toolbar d-sm-flex align-items-center col-12 col-sm-9 col-md-10" style="margin-top: -10px;">
                             <div class="iq-search-bar">
                                 <form action="#" class="searchbox">
                                     <input type="search" class="text search-input" v-model="filter" placeholder="Search">
                                     <a class="search-link" href="#"><i class="ri-search-line"></i></a>
                                 </form>
                             </div>
-                            <iq-card>
+                            <iq-card class="mt-4 mt-sm-0">
                                 <b-form-group label-for="searchOptions" label="Search By:">
                                   <v-select class="patients" label="text"
                                     :clearable="false" :reduce="filter => filter.value"
@@ -253,6 +253,32 @@ export default {
 
 .action-column {
   width: 120px !important;
+}
+
+@media (max-width: 479px) {
+.iq-search-bar {
+  padding: 0 15px 0 0 !important;
+  margin: 0 !important;
+}
+.search-link {
+  right: -4px !important;
+  top: 94px !important;
+}
+.search-input {
+  width: 105% !important;
+}
+}
+
+@media (min-width: 479px) and (max-width: 1400px) {
+.aki {
+  margin-right: -28px !important;
+}
+}
+
+@media (min-width: 1401px) {
+.aki {
+  margin-right: -71px !important;
+}
 }
 
 .patients .vs__search::placeholder,
