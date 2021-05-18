e38694e9c476742b05e16d87f9e67a114462b41c
topsmart20 <oswellgrundmeier42@gmail.com>
2021-05-18 19:42:14 +0200

    Create company service

---
+++ SmartPRM-ClinicModule/src/services/companies.js
@@ -0,0 +1,23 @@
+export async function getCompanies () {
+  const rawResponse = await fetch('/api/companies', {
+    method: 'GET',
+    credentials: 'same-origin',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+  return rawResponse.json()
+}
+
+export async function getCompanyById (id) {
+  const rawResponse = await fetch('/api/companies/' + id, {
+    method: 'GET',
+    credentials: 'same-origin',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+  return rawResponse.json()
+}
