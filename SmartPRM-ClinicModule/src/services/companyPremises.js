9682c2ea36ed53220d0cb0096871d83ade88a31c
topsmart20 <oswellgrundmeier42@gmail.com>
2021-05-23 00:36:17 +0200

    Add service for company premises

---
+++ SmartPRM-ClinicModule/src/services/companyPremises.js
@@ -0,0 +1,11 @@
+export async function getCompanyPremises () {
+  const rawResponse = await fetch('/api/comapny-premises', {
+    method: 'GET',
+    credentials: 'same-origin',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+  return rawResponse.json()
+}
