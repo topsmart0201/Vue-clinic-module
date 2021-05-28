46aaa49f71909a0c36759beec81130a62fd143c4
topsmart20 <oswellgrundmeier42@gmail.com>
2021-05-28 22:13:33 +0300

    get days array for cards

--- SmartPRM-ClinicModule/src/views/Booking/components/Order/ChooseTimeTab.vue
+++ SmartPRM-ClinicModule/src/views/Booking/components/Order/ChooseTimeTab.vue
@@ -1,10 +1,48 @@
 <template>
-  <div class="mt-5">
-    Choose Time
+  <div class="mt-5" v-if="datesList.length > 0">
+    <slick :option="{
+      slidesToShow: 9
+    }">
+    <date-card v-for="(dateItem, index) in datesList" :key="`date_${index}`"
+    :date="dateItem.date"
+    :priceField="dateItem.priceField"
+    />
+    </slick>
   </div>
 </template>
 
 <script>
+import Slick from '@/components/xray/slider/Slick.vue'
+import DateCard from './DateCard.vue'
 export default {
+  components: {
+    Slick,
+    DateCard
+  },
+  data: function () {
+    return {
+      datesList: []
+    }
+  },
+  methods: {
+    getDatesArr: function () {
+      const resArray = []
+      const currDate = new Date(Date.now())
+      const endDate = new Date(currDate.getTime())
+      endDate.setDate(endDate.getDate() + 17)
+      /* eslint no-unmodified-loop-condition: "off" */
+      while (currDate <= endDate) {
+        resArray.push({
+          date: new Date(currDate),
+          priceField: '35$'
+        })
+        currDate.setDate(currDate.getDate() + 1)
+      }
+      return resArray
+    }
+  },
+  mounted () {
+    this.datesList = this.getDatesArr()
+  }
 }
 </script>
