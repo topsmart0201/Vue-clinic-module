81911af73042e62de3b8b050fd861986cd975973
topsmart20 <oswellgrundmeier42@gmail.com>
2021-05-29 17:26:13 +0300

    create time-selectiot-table-row-component

---
+++ SmartPRM-ClinicModule/src/views/Booking/components/Order/TimeSelectionTableRow.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-row>
+    <b-col md="6" lg="4" xl="4" v-for="(doctor, index) in doctors" :key="`title_doctor_${index}`" class="p-1">
+      <div class="doctor-item d-flex align-items-center active-element p-1 rounded-circle" @click="$emit('select-doctor', doctor)">
+      <img class="img-fluid avatar-40 rounded-circle mr-1" :src="doctor.img" :alt="doctor.name">
+      <p class="mb-0">{{doctor.name}}</p>
+      </div>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  props: {
+    doctors: Array
+  }
+}
+</script>
+<style scoped lang="scss">
+@import "~@/assets/scss/app-vars.scss";
+.doctor-item {
+  background-color: $primary-10;
+  color: $primary;
+  transition: all .35s ease-in-out;
+  &:hover{
+    background-color: $primary-20;
+    box-shadow: 1px 1px 3px $gray-300;
+  }
+}
+</style>
