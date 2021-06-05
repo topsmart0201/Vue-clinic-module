840e76495cace67c7012e707fd4513f99c90aff1
topsmart20 <oswellgrundmeier42@gmail.com>
2021-06-05 19:28:06 +0300

    add expander

--- SmartPRM-ClinicModule/src/views/Booking/components/Order/TimeSelectionTableRow.vue
+++ SmartPRM-ClinicModule/src/views/Booking/components/Order/TimeSelectionTableRow.vue
@@ -1,16 +1,21 @@
 <template>
-  <b-row>
-    <b-col md="6" lg="4" xl="4" v-for="(doctor, index) in doctors" :key="`title_doctor_${index}`" class="p-1">
-      <div
-      class="doctor-item d-flex align-items-center active-element p-1 rounded-circle"
-      :class="{'active': !!activeDoctor && activeDoctor.name === doctor.name}"
-      @click="$emit('select-doctor', doctor)"
-      >
-      <img class="img-fluid avatar-40 rounded-circle mr-1" :src="doctor.img" :alt="doctor.name">
-      <p class="mb-0">{{doctor.name}}</p>
-      </div>
-    </b-col>
-  </b-row>
+  <div>
+  <b-container class="time-row" :class="{expanded}" fluid>
+    <b-row ref="timeRow">
+      <b-col md="6" lg="4" xl="4" v-for="(doctor, index) in doctors" :key="`title_doctor_${index}`" class="p-1">
+        <div
+        class="doctor-item d-flex align-items-center active-element p-1 rounded-circle"
+        :class="{'active': !!activeDoctor && activeDoctor.name === doctor.name}"
+        @click="$emit('select-doctor', doctor)"
+        >
+        <img class="img-fluid avatar-40 rounded-circle mr-1" :src="doctor.img" :alt="doctor.name">
+        <p class="mb-0">{{doctor.name}}</p>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
+  <div v-if="contentHeight > 60" class="show-more rounded-circle mt-1" @click="expanded = !expanded">Show more...</div>
+  </div>
 </template>
 
 <script>
@@ -18,11 +23,37 @@ export default {
   props: {
     doctors: Array,
     activeDoctor: Object
+  },
+  data: function () {
+    return {
+      contentHeight: 0,
+      expanded: false
+    }
+  },
+  mounted () {
+    this.setContentHeight(this.$refs.timeRow.offsetHeight)
+  },
+  updated () {
+    this.setContentHeight(this.$refs.timeRow.offsetHeight)
+  },
+  methods: {
+    setContentHeight: function (height) {
+      (this.contentHeight !== height) && (this.contentHeight = height)
+    }
   }
 }
 </script>
 <style scoped lang="scss">
 @import "~@/assets/scss/app-vars.scss";
+.time-row {
+  max-height: 60px;
+  overflow-y: hidden;
+  transition: all .5s linear;
+  animation: 1s open-content;
+  &.expanded{
+    max-height: 250px;
+  }
+}
 .doctor-item {
   background-color: $primary-10;
   color: $primary;
@@ -37,5 +68,10 @@ export default {
     background-color: $primary-60;
     color: $white;
   }
+}
+.show-more{
+  font-size: 12px;
+  background: $gray-100;
+  cursor: pointer;
 }
 </style>
