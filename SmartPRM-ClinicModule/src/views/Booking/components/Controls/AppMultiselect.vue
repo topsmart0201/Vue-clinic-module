b1eb6a0e15a75d5b68c89570de555fa56b282c12
topsmart20 <oswellgrundmeier42@gmail.com>
2021-06-05 09:15:15 +0300

    create app-multiselect component

---
+++ SmartPRM-ClinicModule/src/views/Booking/components/Controls/AppMultiselect.vue
@@ -0,0 +1,38 @@
+<template>
+  <multiselect
+  v-model="selectValue"
+  :options="options"
+  placeholder="Pick some"
+  :multiple="true"
+  :label="label"
+  :track-by="trackBy"
+  >
+  </multiselect>
+</template>
+
+<script>
+import Multiselect from 'vue-multiselect'
+
+export default {
+  components: {
+    Multiselect
+  },
+  props: [
+    'options',
+    'label',
+    'trackBy',
+    'value'
+  ],
+  computed: {
+    selectValue: {
+      get: function () {
+        return this.value
+      },
+      set: function (val) {
+        this.$emit('input', val)
+      }
+    }
+  }
+}
+</script>
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
