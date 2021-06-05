97808fbecba94b685b9b30e89ec1171538975082
topsmart20 <oswellgrundmeier42@gmail.com>
2021-06-05 16:29:02 +0300

    add review-tab table

--- SmartPRM-ClinicModule/src/views/Booking/components/Order/ReviewTab.vue
+++ SmartPRM-ClinicModule/src/views/Booking/components/Order/ReviewTab.vue
@@ -2,6 +2,32 @@
   <div>
     <h5 class="text-left">Check selected slot and confirm reservation.</h5>
     <div class="selected-slot p-2 my-3">Selected slot: {{selectedSlot.date | formatFullDate}} at {{selectedSlot.time}}</div>
+     <b-table
+      :items="services"
+      :fields="fields"
+      responsive="sm"
+    >
+      <template #cell(doctor)>
+        <div class="d-flex align-items-center p-1">
+        <img class="img-fluid avatar-40 rounded-circle mr-1" :src="selectedSlot.doctor.img" :alt="selectedSlot.doctor.name">
+        <p class="mb-0">{{selectedSlot.doctor.name}}</p>
+        </div>
+      </template>
+      <template #cell(time)="data">
+        {{data.value | formatTime}}
+      </template>
+      <template #cell(price)="data">
+        {{data.value | formatMoney}}
+      </template>
+      <template #custom-foot>
+        <tr>
+            <td><b>TOTAL</b></td>
+            <td></td>
+            <td>{{totalTime | formatTime}}</td>
+            <td>{{totalPrice | formatMoney}}</td>
+        </tr>
+    </template>
+    </b-table>
   </div>
 </template>
 
@@ -16,9 +42,18 @@ export default {
   },
   data: function () {
     return {
+      fields: [
+        { key: 'serviceName', label: '' },
+        { key: 'doctor', label: '' },
+        { key: 'time', label: '' },
+        { key: 'price', label: `Start at ${this.selectedSlot.time}` }
+      ]
     }
   },
-  methods: {
+  computed: {
+    totalTime: function () {
+      return this.services.reduce((total, service) => total + service.time, 0)
+    }
   }
 }
 </script>
