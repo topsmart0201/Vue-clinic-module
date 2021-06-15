2828831944760b2164ad9c4015b51c5e9d9d34cc
topsmart20 <oswellgrundmeier42@gmail.com>
2021-06-15 09:19:32 +0200

    working on menu bug

--- SmartPRM-ClinicModule/src/components/xray/sidebars/SideBarStyle1.vue
+++ SmartPRM-ClinicModule/src/components/xray/sidebars/SideBarStyle1.vue
@@ -32,13 +32,34 @@ export default {
   name: 'SideBarStyle1',
   props: {
     homeURL: { type: Object, default: () => ({ name: 'dashboard.home' }) },
-    items: {},
+    items: { type: Array },
     logo: { type: String, default: require('../../../assets/images/logo.png') },
     horizontal: { type: Boolean },
     toggleButton: { type: Boolean, default: true }
   },
   components: {
     List
+  },
+  mounted () {
+    const element = document.querySelectorAll('.iq-sidebar-menu li a')
+    Array.from(element, (elem) => {
+      elem.addEventListener('click', (e) => {
+        const prevMenu = window.$('.iq-sidebar-menu li').has('.iq-submenu').find('ul.show').attr('id')
+        const prevMenuElem = document.getElementById(prevMenu)
+        if (prevMenuElem !== null) {
+          Array.from(prevMenuElem.closest('li').getElementsByClassName('not-collapsed'), (el) => {
+            if (elem.closest('ul').getAttribute('id') !== prevMenu) {
+              this.$root.$emit('bv::toggle::collapse', prevMenu)
+              prevMenuElem.closest('li').classList.remove('active')
+            }
+            if (elem.getAttribute('aria-controls') === prevMenu) {
+              this.$root.$emit('bv::toggle::collapse', prevMenu)
+            }
+          })
+        }
+        window.$(elem).closest('li').has('.iq-submenu').addClass('active')
+      })
+    })
   },
   methods: {
     miniSidebar () {
