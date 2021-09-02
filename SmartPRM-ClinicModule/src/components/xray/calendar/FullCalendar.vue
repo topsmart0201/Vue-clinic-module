05aba22bbc0f7f337a598985302556957f3d4a4f
topsmart20 <oswellgrundmeier42@gmail.com>
2021-09-02 12:52:04 +0200

    reverted bug fix attempt

--- SmartPRM-ClinicModule/src/components/xray/calendar/FullCalendar.vue
+++ SmartPRM-ClinicModule/src/components/xray/calendar/FullCalendar.vue
@@ -542,7 +542,7 @@ export default {
       this.formData.assignmentDate = event.start
       this.formData.end = event.end
       this.formData.doctorId = newResource.title
-      this.formData.time = new Date(event.start)
+      this.formData.time = new Date(event.start).toTimeString()
       event.setExtendedProp('assignmentDate', this.formData.assignmentDate)
       event.setStart(this.formData.start)
       event.setEnd(this.formData.end)
