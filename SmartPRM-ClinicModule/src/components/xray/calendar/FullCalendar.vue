5e57b3951405b780f1e59cff50ec0a4c86227c97
topsmart20 <oswellgrundmeier42@gmail.com>
2021-09-23 21:04:01 +0200

    Fix build

--- SmartPRM-ClinicModule/src/components/xray/calendar/FullCalendar.vue
+++ SmartPRM-ClinicModule/src/components/xray/calendar/FullCalendar.vue
@@ -851,7 +851,7 @@ body .wrapper .custom-control-label::after {
     margin: .225rem !important;
 }
 
-  @import '~@fullcalendar/core/main.css';
+  // @import '~@fullcalendar/core/main.css';
   @import '~@fullcalendar/daygrid/main.css';
   @import '~@fullcalendar/timegrid/main.css';
   @import '~@fullcalendar/list/main.min.css';
