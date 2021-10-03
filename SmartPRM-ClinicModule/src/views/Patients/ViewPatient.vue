f7e030f44e20e27562132586c0233c3b71b1e437
topsmart20 <oswellgrundmeier42@gmail.com>
2021-10-03 22:40:45 +0200

    Add date of sms creation on sms card

--- SmartPRM-ClinicModule/src/views/Patients/ViewPatient.vue
+++ SmartPRM-ClinicModule/src/views/Patients/ViewPatient.vue
@@ -341,7 +341,7 @@
                                                   <li v-for="(message,index) in smsMessages" :key="index + message.created_at" class="d-flex align-items-center justify-content-between mb-3">
                                                       <div>
                                                           <h6>{{message.content}}</h6>
-                                                          <!-- <p class="mb-0">{{note.created_at | formatDate}} - <span class="ml-0">{{ note.user_name }}</span></p> -->
+                                                          <p class="mb-0">{{message.created_at | formatDate}}</p>
                                                       </div>
                                                   </li>
                                               </ul>
