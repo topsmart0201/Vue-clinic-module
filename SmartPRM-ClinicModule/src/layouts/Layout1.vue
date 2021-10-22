449f0a68c28cd612a82df187cdd06e04a9586ddc
topsmart20 <oswellgrundmeier42@gmail.com>
2021-10-22 11:01:20 +0200

    fixed bug where user could not change his/her profile picture

--- SmartPRM-ClinicModule/src/layouts/Layout1.vue
+++ SmartPRM-ClinicModule/src/layouts/Layout1.vue
@@ -246,7 +246,7 @@ export default {
       colors: 'Setting/colorState'
     }),
     getAvatarUrl: function () {
-      return '/api/files/avatar/' + this.logedInUser.id
+      return '/api/files/avatar'
     },
     isEditDisabled () {
       return !this.logedInUser.first_name || !this.logedInUser.surname || !this.logedInUser.email
