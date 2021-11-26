4aff33717ad2763ab6465aca2489993beea99898
topsmart20 <oswellgrundmeier42@gmail.com>
2021-11-26 11:58:20 +0100

    codeCleanUp: calendar

--- SmartPRM-ClinicModule/src/components/xray/calendar/FullCalendar.vue
+++ SmartPRM-ClinicModule/src/components/xray/calendar/FullCalendar.vue
@@ -760,8 +760,6 @@ export default {
       this.$emit('setModalShow', true)
       // this.formData.resourceId = selectionInfo.resource.id
       this.selectedDoctor = this.doctors.find(doctor => doctor.id === +selectionInfo.resource.id)
-      this.selectedPatient = ''
-      this.selectedProductGroup = ''
       // this.formData.doctorId = selectionInfo.resource.title
       // this.formData.eventResourceId = selectionInfo.resource.id
       this.formData.assignmentDate = new Date(selectionInfo.startStr)
