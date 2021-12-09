41cba77e172aa2403826d6a9288cb97c16d673e8
topsmart20 <oswellgrundmeier42@gmail.com>
2021-12-09 12:45:11 +0100

    fix: queries update for getServiceList

--- api/dao/daoReporting.js
+++ api/dao/daoReporting.js
@@ -2,48 +2,48 @@ require('dotenv').config();
 
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORT || 5432,
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT || 5432,
 })
 
-const getEmazingServicesReport = (request, response, startdate, endate, countrie, prm_client_id, scope) =>  {
-    if (scope!='PrmClient' || !prm_client_id) {
+const getEmazingServicesReport = (request, response, startdate, endate, countrie, prm_client_id, scope) => {
+    if (scope != 'PrmClient' || !prm_client_id) {
         response.status(401).json("NOK: user has wrong privileges " + scope)
     }
     if (!countrie || countrie.toLowerCase() == 'all' || countrie.toLowerCase() == 'null') {
         var statement = ["SELECT pr.name as service_title, pr.group, COUNT(pr.name), SUM(se.price), SUM(se.fee) AS fee_sum FROM services se ",
-                     "LEFT JOIN products pr ON se.product_id = pr.id ",
-                     "LEFT JOIN clients cl ON pr.client_id = cl.id ",
-                     "LEFT JOIN clients_prm_client_bridge cpcb ON cl.id = cpcb.clients_id ",                    
-                     "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2 ",
-                     "AND cpcb.prm_client_id=$3 ",
-                     "GROUP BY pr.group, pr.name "].join('\n')
+            "LEFT JOIN products pr ON se.product_id = pr.id ",
+            "LEFT JOIN clients cl ON pr.client_id = cl.id ",
+            "LEFT JOIN clients_prm_client_bridge cpcb ON cl.id = cpcb.clients_id ",
+            "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2 ",
+            "AND cpcb.prm_client_id=$3 ",
+            "GROUP BY pr.group, pr.name "].join('\n')
         pool.query(statement, [startdate, endate, prm_client_id], (error, results) => {
             if (error) {
                 throw error
             }
             response.status(200).json(results.rows)
-        })                     
+        })
     } else {
         var statement = ["SELECT pr.name as service_title, pr.group, COUNT(pr.name), SUM(se.price), SUM(se.fee) AS fee_sum FROM services se ",
-                     "LEFT JOIN products pr ON se.product_id = pr.id ",
-                     "LEFT JOIN enquiries en ON se.enquiry_id = en.id ",
-                     "LEFT JOIN countries co ON en.country_id = co.id ",
-                     "LEFT JOIN clients cl ON pr.client_id = cl.id ",
-                     "LEFT JOIN clients_prm_client_bridge cpcb ON cl.id = cpcb.clients_id ",                     
-                     "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2 AND co.id = $4 ",
-                     "AND cpcb.prm_client_id=$3",
-                     "GROUP BY pr.group, pr.name "].join('\n')
+            "LEFT JOIN products pr ON se.product_id = pr.id ",
+            "LEFT JOIN enquiries en ON se.enquiry_id = en.id ",
+            "LEFT JOIN countries co ON en.country_id = co.id ",
+            "LEFT JOIN clients cl ON pr.client_id = cl.id ",
+            "LEFT JOIN clients_prm_client_bridge cpcb ON cl.id = cpcb.clients_id ",
+            "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2 AND co.id = $4 ",
+            "AND cpcb.prm_client_id=$3",
+            "GROUP BY pr.group, pr.name "].join('\n')
         pool.query(statement, [startdate, endate, prm_client_id, countrie], (error, results) => {
             if (error) {
                 throw error
             }
             response.status(200).json(results.rows)
-        })                      
-    }                       
+        })
+    }
 }
 
 const getServiceList = (request, response, startdate, endate, countrie, prm_client_id, scope) => {
@@ -52,61 +52,61 @@ const getServiceList = (request, response, startdate, endate, countrie, prm_clie
     }
     if (!countrie || countrie.toLowerCase() == 'all' || countrie.toLowerCase() == 'null') {
         var statement = ["SELECT se.date, pr.name as service_title, en.name, en.last_name, co.name AS country, re.name AS region, " +
-         "mu.name AS municipality, se.price, se.fee, us.name as doctor, en.id AS enquiry_id FROM services se ",
-         "LEFT JOIN products pr ON se.product_id = pr.id ",
-         "LEFT JOIN enquiries en ON se.enquiry_id = en.id ",
-         "LEFT JOIN countries co ON en.country_id = co.id ",
-         "LEFT JOIN regions re ON en.region_id = re.id ",
-         "LEFT JOIN municipalities mu ON en.municipality_id = mu.id ",
-         "LEFT JOIN client_users cu ON se.doctor_id = cu.id ",
-         "LEFT JOIN users us ON cu.user_id = us.id ",
-         "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2 ",
-         "AND en.prm_client_id = $3 ",
-         "ORDER BY se.date ASC "].join('\n')
+            "mu.name AS municipality, se.price, se.fee, us.name as doctor, en.id AS enquiry_id FROM services se ",
+            "LEFT JOIN products pr ON se.product_id = pr.id ",
+            "LEFT JOIN enquiries en ON se.enquiry_id = en.id ",
+            "LEFT JOIN countries co ON en.country_id = co.id ",
+            "LEFT JOIN regions re ON en.region_id = re.id ",
+            "LEFT JOIN municipalities mu ON en.municipality_id = mu.id ",
+            "LEFT JOIN client_users cu ON se.doctor_id = cu.id ",
+            "LEFT JOIN users us ON cu.user_id = us.id ",
+            "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2 ",
+            "AND en.prm_client_id = $3 ",
+            "ORDER BY se.date ASC "].join('\n')
         pool.query(statement, [startdate, endate, prm_client_id], (error, results) => {
             if (error) {
                 throw error
             }
             response.status(200).json(results.rows)
-        })                      
+        })
     } else {
         var statement = ["SELECT se.date, pr.name as service_title, en.name, en.last_name, co.name AS country, re.name AS region, " +
-                     "mu.name AS municipality, se.price, se.fee, us.name as doctor, en.id AS enquiry_id FROM services se ",
-                     "LEFT JOIN products pr ON se.product_id = pr.id ",
-                     "LEFT JOIN enquiries en ON se.enquiry_id = en.id ",
-                     "LEFT JOIN countries co ON en.country_id = co.id ",
-                     "LEFT JOIN regions re ON en.region_id = re.id ",
-                     "LEFT JOIN municipalities mu ON en.municipality_id = mu.id ",
-                     "LEFT JOIN client_users cu ON se.doctor_id = cu.id ",
-                     "LEFT JOIN users us ON cu.user_id = us.id ",
-                     "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2 AND co.id = $3 ",
-                     "AND en.prm_client_id = $4 ",
-                     "ORDER BY se.date ASC "].join('\n')
+            "mu.name AS municipality, se.price, se.fee, us.name as doctor, en.id AS enquiry_id FROM services se ",
+            "LEFT JOIN products pr ON se.product_id = pr.id ",
+            "LEFT JOIN enquiries en ON se.enquiry_id = en.id ",
+            "LEFT JOIN countries co ON en.country_id = co.id ",
+            "LEFT JOIN regions re ON en.region_id = re.id ",
+            "LEFT JOIN municipalities mu ON en.municipality_id = mu.id ",
+            "LEFT JOIN client_users cu ON se.doctor_id = cu.id ",
+            "LEFT JOIN users us ON cu.user_id = us.id ",
+            "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2 AND co.id = $3 ",
+            "AND en.prm_client_id = $4 ",
+            "ORDER BY se.date ASC "].join('\n')
         pool.query(statement, [startdate, endate, countrie, prm_client_id], (error, results) => {
             if (error) {
                 throw error
             }
             response.status(200).json(results.rows)
-        })                      
-    }                      
+        })
+    }
 }
 
-const getCountryList = (request, response, startdate, endate) =>  {
+const getCountryList = (request, response, startdate, endate) => {
     var statement = ["SELECT co.id, co.code, co.name FROM services se ",
-                     "LEFT JOIN products pr ON se.product_id = pr.id ",
-                     "LEFT JOIN enquiries en ON se.enquiry_id = en.id ",
-                     "LEFT JOIN countries co ON en.country_id = co.id ",
-                     "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2",
-                     "GROUP BY co.id "].join('\n')
+        "LEFT JOIN products pr ON se.product_id = pr.id ",
+        "LEFT JOIN enquiries en ON se.enquiry_id = en.id ",
+        "LEFT JOIN countries co ON en.country_id = co.id ",
+        "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2",
+        "GROUP BY co.id "].join('\n')
     pool.query(statement, [startdate, endate], (error, results) => {
         if (error) {
             throw error
         }
         response.status(200).json(results.rows)
-    })  
+    })
 }
 
-const getClinicStatistics = async (request, response, clinicId) =>  {
+const getClinicStatistics = async (request, response, clinicId) => {
     let statsRecord = {
         today: [],
         weekly: [],
@@ -119,9 +119,9 @@ const getClinicStatistics = async (request, response, clinicId) =>  {
     var sixtyWeekStatement = ["SELECT date_trunc('week',date)::date as date, SUM(cash_amount) AS cachRevenue, SUM(credit_card_amount) AS cardRevenue, SUM(cash_amount) + SUM(credit_card_amount) AS totalRevenue FROM services where date > current_date - interval '60 weeks' GROUP BY ( date_trunc('week', date)) ORDER BY ( date_trunc('week', date)) ASC "].join('\n')
     var weeklyStatement = ["SELECT date_trunc('week',date)::date as date, SUM(cash_amount) AS cachRevenue, SUM(credit_card_amount) AS cardRevenue, SUM(cash_amount) + SUM(credit_card_amount) AS totalRevenue FROM services where date > current_date - interval '1 week' GROUP BY ( date_trunc('week', date)) ORDER BY ( date_trunc('week', date)) ASC "].join('\n')
     var monthlyStatement = ["SELECT date_trunc('month',date)::date as date, SUM(cash_amount) AS cachRevenue, SUM(credit_card_amount) AS cardRevenue, SUM(cash_amount) + SUM(credit_card_amount) AS totalRevenue FROM services where date > current_date - interval '1 month' GROUP BY ( date_trunc('month', date)) ORDER BY ( date_trunc('month', date)) ASC "].join('\n')
-    var yearlyStatement = ["SELECT date_trunc('year',date)::date as date, SUM(cash_amount) AS cachRevenue, SUM(credit_card_amount) AS cardRevenue, SUM(cash_amount) + SUM(credit_card_amount) AS totalRevenue FROM services where date > current_date - interval '1 year' GROUP BY ( date_trunc('year', date)) ORDER BY ( date_trunc('year', date)) ASC "].join('\n') 
-    
-    await Promise.all([ pool.query(weeklyStatement), pool.query(monthlyStatement), pool.query(yearlyStatement), pool.query(sixtyWeekStatement), pool.query(todayStatement) ]).then(result => {
+    var yearlyStatement = ["SELECT date_trunc('year',date)::date as date, SUM(cash_amount) AS cachRevenue, SUM(credit_card_amount) AS cardRevenue, SUM(cash_amount) + SUM(credit_card_amount) AS totalRevenue FROM services where date > current_date - interval '1 year' GROUP BY ( date_trunc('year', date)) ORDER BY ( date_trunc('year', date)) ASC "].join('\n')
+
+    await Promise.all([pool.query(weeklyStatement), pool.query(monthlyStatement), pool.query(yearlyStatement), pool.query(sixtyWeekStatement), pool.query(todayStatement)]).then(result => {
         statsRecord.weekly = result[0].rows
         statsRecord.monthly = result[1].rows
         statsRecord.yearly = result[2].rows
@@ -134,11 +134,11 @@ const getClinicStatistics = async (request, response, clinicId) =>  {
             message: error.message || 'Something went wrong.'
         })
     })
-} 
+}
 
 module.exports = {
-  getEmazingServicesReport,
-  getServiceList,
-  getCountryList,
-  getClinicStatistics
+    getEmazingServicesReport,
+    getServiceList,
+    getCountryList,
+    getClinicStatistics
 }
