b95feb5294228e20f5dc9568165191c3a53358b6
topsmart20 <oswellgrundmeier42@gmail.com>
2021-12-15 13:41:08 +0100

    fix/wip: Refactoring BE code for reporting

--- api/dao/daoReporting.js
+++ api/dao/daoReporting.js
@@ -10,36 +10,45 @@ const pool = new Pool({
 })
 
 const getEmazingServicesReport = (request, response, startdate, endate, countrie, prm_client_id, scope) => {
-    if (scope != 'PrmClient' || !prm_client_id) {
-        response.status(401).json("NOK: user has wrong privileges " + scope)
-    }
     if (!countrie || countrie.toLowerCase() == 'all' || countrie.toLowerCase() == 'null') {
-        var statement = ["SELECT pr.name as service_title, pr.group, COUNT(pr.name), SUM(se.price), SUM(se.fee) AS fee_sum FROM services se ",
-            "LEFT JOIN products pr ON se.product_id = pr.id ",
-            "LEFT JOIN clients cl ON pr.client_id = cl.id ",
-            "LEFT JOIN clients_prm_client_bridge cpcb ON cl.id = cpcb.clients_id ",
-            "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2 ",
-            "AND cpcb.prm_client_id=$3 ",
-            "GROUP BY pr.group, pr.name "].join('\n')
-        pool.query(statement, [startdate, endate, prm_client_id], (error, results) => {
+        let statement = "SELECT pr.name AS service_title, pr.group, COUNT(pr.name), SUM(se.price), SUM(se.fee) AS fee_sum FROM services se "
+        statement += "LEFT JOIN products pr ON se.product_id = pr.id "
+        statement += "LEFT JOIN clients cl ON pr.client_id = cl.id "
+        statement += "LEFT JOIN clients_prm_client_bridge cpcb ON cl.id = cpcb.clients_id "
+        statement += "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2 "
+        if (scope == 'All') {
+        } else if (scope == 'PrmClient') {
+            statement += "AND cpcb.prm_client_id = " + prm_client_id
+        }
+        statement += "GROUP BY pr.group, pr.name "
+        console.log("Fetching reporting on BE: " + statement)
+        pool.query(statement, [startdate, endate], (error, results) => {
             if (error) {
-                throw error
+                response.status(500).json({
+                    message: error.message || 'Something went wrong.'
+                })
             }
             response.status(200).json(results.rows)
         })
     } else {
-        var statement = ["SELECT pr.name as service_title, pr.group, COUNT(pr.name), SUM(se.price), SUM(se.fee) AS fee_sum FROM services se ",
-            "LEFT JOIN products pr ON se.product_id = pr.id ",
-            "LEFT JOIN enquiries en ON se.enquiry_id = en.id ",
-            "LEFT JOIN countries co ON en.country_id = co.id ",
-            "LEFT JOIN clients cl ON pr.client_id = cl.id ",
-            "LEFT JOIN clients_prm_client_bridge cpcb ON cl.id = cpcb.clients_id ",
-            "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2 AND co.id = $4 ",
-            "AND cpcb.prm_client_id=$3",
-            "GROUP BY pr.group, pr.name "].join('\n')
-        pool.query(statement, [startdate, endate, prm_client_id, countrie], (error, results) => {
+        let statement = "SELECT pr.name as service_title, pr.group, COUNT(pr.name), SUM(se.price), SUM(se.fee) AS fee_sum FROM services se "
+        statement += "LEFT JOIN products pr ON se.product_id = pr.id "
+        statement += "LEFT JOIN enquiries en ON se.enquiry_id = en.id "
+        statement += "LEFT JOIN countries co ON en.country_id = co.id "
+        statement += "LEFT JOIN clients cl ON pr.client_id = cl.id "
+        statement += "LEFT JOIN clients_prm_client_bridge cpcb ON cl.id = cpcb.clients_id "
+        statement += "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2 AND co.id = $4 "
+        if (scope == 'All') {
+        } else if (scope == 'PrmClient') {
+        statement += "AND cpcb.prm_client_id= " + prm_client_id
+        }
+        statement += "GROUP BY pr.group, pr.name "
+        console.log("Fetching reporting on BE: " + statement)
+        pool.query(statement, [startdate, endate, countrie], (error, results) => {
             if (error) {
-                throw error
+                response.status(500).json({
+                    message: error.message || 'Something went wrong.'
+                })
             }
             response.status(200).json(results.rows)
         })
@@ -47,44 +56,53 @@ const getEmazingServicesReport = (request, response, startdate, endate, countrie
 }
 
 const getServiceList = (request, response, startdate, endate, countrie, prm_client_id, scope) => {
-    if (scope != 'PrmClient' || !prm_client_id) {
-        response.status(401).json("NOK: user has wrong privileges " + scope)
-    }
     if (!countrie || countrie.toLowerCase() == 'all' || countrie.toLowerCase() == 'null') {
-        var statement = ["SELECT se.date, pr.name as service_title, en.name, en.last_name, co.name AS country, re.name AS region, " +
-            "mu.name AS municipality, se.price, se.fee, us.name as doctor, en.id AS enquiry_id FROM services se ",
-            "LEFT JOIN products pr ON se.product_id = pr.id ",
-            "LEFT JOIN enquiries en ON se.enquiry_id = en.id ",
-            "LEFT JOIN countries co ON en.country_id = co.id ",
-            "LEFT JOIN regions re ON en.region_id = re.id ",
-            "LEFT JOIN municipalities mu ON en.municipality_id = mu.id ",
-            "LEFT JOIN client_users cu ON se.doctor_id = cu.id ",
-            "LEFT JOIN users us ON cu.user_id = us.id ",
-            "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2 ",
-            "AND en.prm_client_id = $3 ",
-            "ORDER BY se.date ASC "].join('\n')
-        pool.query(statement, [startdate, endate, prm_client_id], (error, results) => {
+        let statement = "SELECT se.date, pr.name as service_title, en.name, en.last_name, co.name AS country, re.name AS region, " +
+            "mu.name AS municipality, se.price, se.fee, us.name as doctor, en.id AS enquiry_id FROM services se "
+        statement += "LEFT JOIN products pr ON se.product_id = pr.id "
+        statement += "LEFT JOIN enquiries en ON se.enquiry_id = en.id "
+        statement += "LEFT JOIN countries co ON en.country_id = co.id "
+        statement += "LEFT JOIN regions re ON en.region_id = re.id "
+        statement += "LEFT JOIN municipalities mu ON en.municipality_id = mu.id "
+        statement += "LEFT JOIN client_users cu ON se.doctor_id = cu.id "
+        statement += "LEFT JOIN users us ON cu.user_id = us.id "
+        statement += "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2 "
+        if (scope == 'All') {
+        } else if (scope == 'PrmClient') {
+            statement += " AND en.prm_client_id = " + prm_client_id
+        }
+        statement += "ORDER BY se.date ASC "
+        console.log("Fetching reporting on BE: " + statement)
+        pool.query(statement, [startdate, endate], (error, results) => {
             if (error) {
-                throw error
+                response.status(500).json({
+                    message: error.message || 'Something went wrong.'
+                })
             }
             response.status(200).json(results.rows)
         })
     } else {
-        var statement = ["SELECT se.date, pr.name as service_title, en.name, en.last_name, co.name AS country, re.name AS region, " +
-            "mu.name AS municipality, se.price, se.fee, us.name as doctor, en.id AS enquiry_id FROM services se ",
-            "LEFT JOIN products pr ON se.product_id = pr.id ",
-            "LEFT JOIN enquiries en ON se.enquiry_id = en.id ",
-            "LEFT JOIN countries co ON en.country_id = co.id ",
-            "LEFT JOIN regions re ON en.region_id = re.id ",
-            "LEFT JOIN municipalities mu ON en.municipality_id = mu.id ",
-            "LEFT JOIN client_users cu ON se.doctor_id = cu.id ",
-            "LEFT JOIN users us ON cu.user_id = us.id ",
-            "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2 AND co.id = $3 ",
-            "AND en.prm_client_id = $4 ",
-            "ORDER BY se.date ASC "].join('\n')
-        pool.query(statement, [startdate, endate, countrie, prm_client_id], (error, results) => {
+        let statement = "SELECT se.date, pr.name as service_title, en.name, en.last_name, co.name AS country, re.name AS region, " +
+            "mu.name AS municipality, se.price, se.fee, us.name as doctor, en.id AS enquiry_id FROM services se "
+        statement += "LEFT JOIN products pr ON se.product_id = pr.id "
+        statement += "LEFT JOIN enquiries en ON se.enquiry_id = en.id ",
+        statement += "LEFT JOIN countries co ON en.country_id = co.id ",
+        statement += "LEFT JOIN regions re ON en.region_id = re.id ",
+        statement += "LEFT JOIN municipalities mu ON en.municipality_id = mu.id ",
+        statement += "LEFT JOIN client_users cu ON se.doctor_id = cu.id ",
+        statement += "LEFT JOIN users us ON cu.user_id = us.id ",
+        statement += "WHERE date_trunc('day', se.date) >= $1 AND (date_trunc('day', se.date) - INTERVAL '1 DAY' ) <= $2 AND co.id = $3 "
+        if (scope == 'All') {
+        } else if (scope == 'PrmClient') {
+            "AND en.prm_client_id = " + prm_client_id
+        }
+        statement += "ORDER BY se.date ASC "
+        console.log("Fetching reporting on BE: " + statement)
+        pool.query(statement, [startdate, endate, countrie], (error, results) => {
             if (error) {
-                throw error
+                response.status(500).json({
+                    message: error.message || 'Something went wrong.'
+                })
             }
             response.status(200).json(results.rows)
         })
