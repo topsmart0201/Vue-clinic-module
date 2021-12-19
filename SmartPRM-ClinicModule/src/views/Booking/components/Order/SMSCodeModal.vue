dfe23c790c8032ddd5ebee0a21d12aa06acb1294
topsmart20 <oswellgrundmeier42@gmail.com>
2021-12-19 22:59:51 +0700

    feat: booking - add name field

--- SmartPRM-ClinicModule/src/views/Booking/components/Order/SMSCodeModal.vue
+++ SmartPRM-ClinicModule/src/views/Booking/components/Order/SMSCodeModal.vue
@@ -7,27 +7,64 @@
     :title="$t('public.onlineBooking.confirmationOfBooking')"
   >
     <template v-if="fieldset.verificationId == null">
-      <p>
-        {{ $t('public.onlineBooking.pleaseEnterYourMobileNumber') }}
-      </p>
-      <ValidationProvider
-        :name="$t('public.onlineBooking.phone')"
-        rules="required|numeric|min:9|max:15"
-        v-slot="{ errors, invalid }"
-      >
-        <b-form-group :label="$t('public.onlineBooking.phoneNumber')">
-          <b-form-input
-            v-model="fieldset.phone" type="text"
-            :class="(errors.length > 0 ? ' is-invalid' : '')"
-          />
-          <div class="invalid-feedback">
-            <span>{{ errors[0] }}</span>
+      <ValidationObserver v-slot="{ invalid, validate }">
+        <form @submit.prevent="validate().then(sendConfirmationCode)">
+          <div class="d-flex">
+            <ValidationProvider
+              :name="$t('public.onlineBooking.firstName')"
+              rules="required"
+              v-slot="{ errors }"
+            >
+              <b-form-group :label="$t('public.onlineBooking.firstName')">
+                <b-form-input
+                  v-model="fieldset.firstName" type="text"
+                  :class="(errors.length > 0 ? ' is-invalid' : '')"
+                />
+                <div class="invalid-feedback">
+                  <span>{{ errors[0] }}</span>
+                </div>
+              </b-form-group>
+            </ValidationProvider>
+            <div style="width: 16px"></div>
+            <ValidationProvider
+              :name="$t('public.onlineBooking.lastName')"
+              rules="required"
+              v-slot="{ errors }"
+            >
+              <b-form-group :label="$t('public.onlineBooking.lastName')">
+                <b-form-input
+                  v-model="fieldset.lastName" type="text"
+                  :class="(errors.length > 0 ? ' is-invalid' : '')"
+                />
+                <div class="invalid-feedback">
+                  <span>{{ errors[0] }}</span>
+                </div>
+              </b-form-group>
+            </ValidationProvider>
           </div>
-        </b-form-group>
-        <b-button variant="primary" :disabled="invalid" @click="sendConfirmationCode">
-          {{ $t('public.onlineBooking.sendVerificationCode') }}
-        </b-button>
-      </ValidationProvider>
+          <ValidationProvider
+            :name="$t('public.onlineBooking.phone')"
+            rules="required|numeric|min:9|max:15"
+            v-slot="{ errors, invalid }"
+          >
+            <p>
+              {{ $t('public.onlineBooking.pleaseEnterYourMobileNumber') }}
+            </p>
+            <b-form-group :label="$t('public.onlineBooking.phoneNumber')">
+              <b-form-input
+                v-model="fieldset.phone" type="text"
+                :class="(errors.length > 0 ? ' is-invalid' : '')"
+              />
+              <div class="invalid-feedback">
+                <span>{{ errors[0] }}</span>
+              </div>
+            </b-form-group>
+          </ValidationProvider>
+          <b-button type="submit" variant="primary" :disabled="invalid">
+            {{ $t('public.onlineBooking.sendVerificationCode') }}
+          </b-button>
+        </form>
+      </ValidationObserver>
     </template>
 
     <template v-if="fieldset.verificationId != null">
