efa4ac9ee11e60b7df1c2815cf9e14b65800cfae
topsmart20 <oswellgrundmeier42@gmail.com>
2022-01-06 12:43:49 +0100

    fix: adjusted getPatients method

--- api/server.js
+++ api/server.js
@@ -750,14 +750,14 @@ app.put('/api/update-online-booking-service/:id', (req, res) => {
 app.get('/api/enquiries', (req, res) => {
   let sortBy = req.query.sort
   if(req.session.prm_user && req.session.prm_user.permissions && checkPermission(req.session.prm_user.permissions, enquiriesPermission))
-      daoEnquiries.getEnquiries(req, res, req.session.prm_user.id, req.session.prm_user.accessible_user_ids, req.session.prm_user.prm_client_id, getScope(req.session.prm_user.permissions, calendarPermission), sortBy)
+      daoEnquiries.getEnquiries(req, res, req.session.prm_user.id, req.session.prm_user.accessible_user_ids, req.session.prm_user.prm_client_id, getScope(req.session.prm_user.permissions, enquiriesPermission), sortBy)
   else
       res.status(401).json("OK: user unauthorized")
 });
 
 app.get('/api/patients', (req, res) => {
     if(req.session.prm_user && req.session.prm_user.permissions && checkPermission(req.session.prm_user.permissions, enquiriesPermission))
-        daoEnquiries.getPatients(req, res)
+        daoEnquiries.getPatients(req, res, getScope(req.session.prm_user.permissions, enquiriesPermission), req.session.prm_user.prm_client_id, req.session.prm_user.id, req.session.prm_user.accessible_user_ids)
     else
         res.status(401).json("OK: user unauthorized")
   });
