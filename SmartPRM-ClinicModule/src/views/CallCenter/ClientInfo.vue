5599977fecc3229f0b4f5076960083cac0e777a7
topsmart20 <oswellgrundmeier42@gmail.com>
2022-01-11 19:14:59 +0500

    chore: client info textarea added

--- SmartPRM-ClinicModule/src/views/CallCenter/ClientInfo.vue
+++ SmartPRM-ClinicModule/src/views/CallCenter/ClientInfo.vue
@@ -1,13 +1,24 @@
 <template>
     <div class="container">
-        <h1>Client Info</h1>
+        <iq-card class-name="iq-card-block iq-card-stretch iq-card-height">
+          <template v-slot:headerTitle>
+            <h2 class="card-title">Client Info</h2>
+          </template>
+          <template v-slot:body>
+            <vue-editor v-model="content"></vue-editor>
+          </template>
+        </iq-card>
     </div>
 </template>
 
 <script>
 import { xray } from '../../config/pluginInit'
+import IqCard from '../../components/xray/cards/iq-card'
+import { VueEditor } from 'vue2-editor'
 export default {
   components: {
+    IqCard,
+    VueEditor,
   },
   name: 'ClientInfo',
   mounted() {
@@ -15,6 +26,11 @@ export default {
   },
   data: function () {
     return {
+      content: '<p>Edit Client Info</p>',
+      customToolbar: [
+        ['paragraph', 'bold', 'italic', 'underline', 'link'],
+        [{ list: 'ordered' }, { list: 'bullet' }],
+      ],
     }
   },
   methods: {
