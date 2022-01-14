9495d399aafdadeef307f6f4f8fa95167298b59b
topsmart20 <oswellgrundmeier42@gmail.com>
2022-01-14 19:40:56 +0700

    style: fix

--- SmartPRM-ClinicModule/src/store/Setting/index.js
+++ SmartPRM-ClinicModule/src/store/Setting/index.js
@@ -6,10 +6,22 @@ import SideBarItems from '../../FackApi/json/GlobalSearch'
 const state = {
   horizontalMenu: false,
   miniSidebarMenu: false,
-  lang: { title: 'English', value: 'en', image: require('../../assets/images/small/flag-11.png') },
+  lang: {
+    title: 'English',
+    value: 'en',
+    image: require('../../assets/images/small/flag-11.png'),
+  },
   langOption: [
-    { title: 'English', value: 'en', image: require('../../assets/images/small/flag-11.png') },
-    { title: 'Slovenščina', value: 'sl', image: require('../../assets/images/small/flag-800.png') },
+    {
+      title: 'English',
+      value: 'en',
+      image: require('../../assets/images/small/flag-11.png'),
+    },
+    {
+      title: 'Slovenščina',
+      value: 'sl',
+      image: require('../../assets/images/small/flag-800.png'),
+    },
     /* { title: 'Arabic', value: 'ar', image: require('../../assets/images/small/flag-500.png') },
     { title: 'Chinese', value: 'chi', image: require('../../assets/images/small/flag-300.png') },
     { title: 'Hindi', value: 'hi', image: require('../../assets/images/small/flag-100.png') },
@@ -17,13 +29,48 @@ const state = {
     { title: 'Franch', value: 'fr', image: require('../../assets/images/small/flag-200.png') } */
   ],
   colors: [
-    { primary: '#827af3', primaryLight: '#b47af3', bodyBgLight: '#efeefd', bodyBgDark: '#1d203f' },
-    { primary: '#e07af3', primaryLight: '#f37ab7', bodyBgLight: '#f7eefd', bodyBgDark: '#1d203f' },
-    { primary: '#6475c7', primaryLight: '#7abbf3', bodyBgLight: '#eaf5ff', bodyBgDark: '#1d203f' },
-    { primary: '#c76464', primaryLight: '#f3c37a', bodyBgLight: '#fff8ea', bodyBgDark: '#1d203f' },
-    { primary: '#c764ad', primaryLight: '#de8ba9', bodyBgLight: '#ffeaf5', bodyBgDark: '#1d203f' },
-    { primary: '#64c7ac', primaryLight: '#a3f37a', bodyBgLight: '#f0ffea', bodyBgDark: '#1d203f' },
-    { primary: '#8ac764', primaryLight: '#dbf37a', bodyBgLight: '#f7ffea', bodyBgDark: '#1d203f' },
+    {
+      primary: '#827af3',
+      primaryLight: '#b47af3',
+      bodyBgLight: '#efeefd',
+      bodyBgDark: '#1d203f',
+    },
+    {
+      primary: '#e07af3',
+      primaryLight: '#f37ab7',
+      bodyBgLight: '#f7eefd',
+      bodyBgDark: '#1d203f',
+    },
+    {
+      primary: '#6475c7',
+      primaryLight: '#7abbf3',
+      bodyBgLight: '#eaf5ff',
+      bodyBgDark: '#1d203f',
+    },
+    {
+      primary: '#c76464',
+      primaryLight: '#f3c37a',
+      bodyBgLight: '#fff8ea',
+      bodyBgDark: '#1d203f',
+    },
+    {
+      primary: '#c764ad',
+      primaryLight: '#de8ba9',
+      bodyBgLight: '#ffeaf5',
+      bodyBgDark: '#1d203f',
+    },
+    {
+      primary: '#64c7ac',
+      primaryLight: '#a3f37a',
+      bodyBgLight: '#f0ffea',
+      bodyBgDark: '#1d203f',
+    },
+    {
+      primary: '#8ac764',
+      primaryLight: '#dbf37a',
+      bodyBgLight: '#f7ffea',
+      bodyBgDark: '#1d203f',
+    },
   ],
   authUser: {
     auth: false,
@@ -44,19 +91,19 @@ const state = {
   bookmark: [
     {
       title: 'Video Chat',
-      link: { 'name': 'app.chat' },
+      link: { name: 'app.chat' },
       is_icon_class: true,
       icon: 'ri-message-line',
     },
     {
       title: 'Product Listing',
-      link: { 'name': 'app.e-commerce.index' },
+      link: { name: 'app.e-commerce.index' },
       is_icon_class: true,
       icon: 'ri-file-list-line',
     },
     {
       title: 'Social App',
-      link: { 'name': 'social.list' },
+      link: { name: 'social.list' },
       is_icon_class: true,
       icon: 'ri-question-answer-line',
     },
@@ -68,11 +115,10 @@ const state = {
     },
     {
       title: 'Inbox',
-      link: { 'name': 'app.email' },
+      link: { name: 'app.email' },
       is_icon_class: true,
       icon: 'ri-inbox-line',
     },
-
   ],
   activePage: {
     name: 'Dashboard',
