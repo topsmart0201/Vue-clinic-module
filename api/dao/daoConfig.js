1d6251c839e9d18671c6a9cb24b2703dfa0dfa43
topsmart20 <oswellgrundmeier42@gmail.com>
2022-01-14 19:35:17 +0700

    style: fix

--- api/dao/daoConfig.js
+++ api/dao/daoConfig.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config()
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
@@ -9,56 +9,57 @@ const pool = new Pool({
 })
 
 const langByCountry = {
-    'SI': 'sl'
+  SI: 'sl',
 }
 
 const getConfig = (request, response, ip, premiseId) => {
-    const geoip = require('geoip-lite')
-    const geo = geoip.lookup(ip)
-    const lang = geo != null && langByCountry[geo.country] != null
-        ? langByCountry[geo.country]
-        : 'en'
+  const geoip = require('geoip-lite')
+  const geo = geoip.lookup(ip)
+  const lang =
+    geo != null && langByCountry[geo.country] != null
+      ? langByCountry[geo.country]
+      : 'en'
 
-    const statement = /* sql */`
-        SELECT
-            prm_company_premise.premise_name as "name",
-            prm_company_premise.premise_street as street,
-            prm_company_premise.premise_house_number as house_number,
-            prm_company_premise.premise_post_code as post_code,
-            prm_company_premise.premise_phone_number as phone_number,
-            prm_company_premise.premise_city as city,
-            prm_company.prm_client_id as client_id,
-            prm_company.company_name as company_name
-        FROM prm_company_premise
-        JOIN prm_company ON prm_company_premise.company_id = prm_company.company_id
-        WHERE premise_id = $1
-        LIMIT 1
-    `
+  const statement = /* sql */ `
+    SELECT
+      prm_company_premise.premise_name as "name",
+      prm_company_premise.premise_street as street,
+      prm_company_premise.premise_house_number as house_number,
+      prm_company_premise.premise_post_code as post_code,
+      prm_company_premise.premise_phone_number as phone_number,
+      prm_company_premise.premise_city as city,
+      prm_company.prm_client_id as client_id,
+      prm_company.company_name as company_name
+    FROM prm_company_premise
+    JOIN prm_company ON prm_company_premise.company_id = prm_company.company_id
+    WHERE premise_id = $1
+    LIMIT 1
+  `
 
-    pool.query(statement, [premiseId], (error, result) => {
-        if (error) {
-            response.status(500).send()
+  pool.query(statement, [premiseId], (error, result) => {
+    if (error) {
+      response.status(500).send()
 
-            return
-        }
+      return
+    }
 
-        const premise = result.rows[0]
+    const premise = result.rows[0]
 
-        response.json({
-            lang,
-            premise: {
-                name: premise.name,
-                address: `${premise.street} ${premise.house_number}, ${premise.post_code} ${premise.city}`,
-                client_id: premise.client_id,
-                company_name: premise.company_name,
-                phone_number: premise.phone_number,
-            }
-        })
+    response.json({
+      lang,
+      premise: {
+        name: premise.name,
+        address: `${premise.street} ${premise.house_number}, ${premise.post_code} ${premise.city}`,
+        client_id: premise.client_id,
+        company_name: premise.company_name,
+        phone_number: premise.phone_number,
+      },
     })
+  })
 }
 
 const daoConfig = {
-    getConfig
+  getConfig,
 }
 
 module.exports = daoConfig
