3fdb9c0a898ededbd3832b8340719d71f307fae5
topsmart20 <oswellgrundmeier42@gmail.com>
2022-01-17 13:41:52 +0500

    chore: Missing services page finished

--- SmartPRM-ClinicModule/src/views/CallCenter/MissingServices.vue
+++ SmartPRM-ClinicModule/src/views/CallCenter/MissingServices.vue
@@ -2,7 +2,67 @@
   <b-container fluid>
     <iq-card class-name="iq-card-block iq-card-stretch iq-card-height">
       <template v-slot:headerTitle>
-        <h2 class="card-title">Missing Services</h2>
+        <h2 class="card-title mt-3">Missing Services</h2>
+        <b-row>
+          <b-col cols="12" sm="6" md="4" lg="3">
+            <b-form-group>
+              <label style="padding-top: 8px">Filter</label>
+              <b-form-select
+                style="line-height: normal"
+                v-model="filterBy"
+                @change="onFilterChange"
+              >
+                <b-form-select-option :value="null"
+                  >Select Filter</b-form-select-option
+                >
+                <b-form-select-option :value="7"
+                  >Last 7 Days</b-form-select-option
+                >
+                <b-form-select-option :value="30"
+                  >Last 30 Days</b-form-select-option
+                >
+                <b-form-select-option :value="90"
+                  >Last 90 Days</b-form-select-option
+                >
+                <b-form-select-option :value="180"
+                  >Last 180 Days</b-form-select-option
+                >
+                <b-form-select-option :value="365"
+                  >Last 365 Days</b-form-select-option
+                >
+                <b-form-select-option :value="2"
+                  >Last 2 Years</b-form-select-option
+                >
+              </b-form-select>
+            </b-form-group>
+          </b-col>
+          <b-col cols="12" sm="6" md="4" lg="3">
+            <b-form-group>
+              <label style="padding-top: 8px">From:</label>
+              <b-form-input
+                style="line-height: normal"
+                class="date"
+                id="exampleStartdate"
+                type="date"
+                v-model="startDate"
+                @change="onDateChange"
+              ></b-form-input>
+            </b-form-group>
+          </b-col>
+          <b-col cols="12" sm="6" md="4" lg="3">
+            <b-form-group>
+              <label style="padding-top: 8px">End:</label>
+              <b-form-input
+                style="line-height: normal"
+                class="date"
+                id="exampleEnddate"
+                type="date"
+                v-model="endDate"
+                @change="onDateChange"
+              ></b-form-input>
+            </b-form-group>
+          </b-col>
+        </b-row>
       </template>
       <template v-slot:body>
         <b-table-simple responsive bordered stripped>
@@ -15,13 +75,31 @@
             </b-tr>
           </b-thead>
           <b-tbody>
-            <b-tr v-for="i in 5" :key="i">
+            <b-tr v-for="(appointment, index) in appointments" :key="index">
               <b-td>
-                <router-link to="/call-center/leads">Branka Marolt Majcen</router-link>
+                <router-link v-if="appointment.lead_name" :to="`/patients/${appointment.lead_id}`">{{ appointment.lead_name }}</router-link>
+                <span v-else>N/A</span>
               </b-td>
-              <b-td>Lucija Zamlic</b-td>
-              <b-td>2021-12-14</b-td>
-              <b-td></b-td>
+              <b-td>{{ appointment.lead_owner || 'N/A' }}</b-td>
+              <b-td>{{ appointment.appointment_date && appointment.appointment_date.split('T')[0] }}</b-td>
+              <b-td>{{ getStatus(appointment.appointment_status) }}</b-td>
+            </b-tr>
+            <b-tr v-if="loading">
+              <b-td colspan="4">
+                <div class="mt-3 text-center">
+                  <div class="text-center text-primary my-2">
+                      <b-spinner class="align-middle"></b-spinner>
+                      <strong class="loading">Loading...</strong>
+                  </div>
+                </div>
+              </b-td>
+            </b-tr>
+            <b-tr v-if="!loading && !appointments.length">
+              <b-td colspan="4">
+                <div class="mt-3 text-center">
+                  <p>No data found in this date range...</p>
+                </div>
+              </b-td>
             </b-tr>
           </b-tbody>
         </b-table-simple>
@@ -33,6 +111,9 @@
 <script>
 import { xray } from '../../config/pluginInit'
 import IqCard from '../../components/xray/cards/iq-card'
+import { getAppointmentsWithoutServices } from '../../services/callCenterService'
+import moment from 'moment'
+
 export default {
   components: {
     IqCard,
@@ -40,12 +121,66 @@ export default {
   name: 'MissingServices',
   mounted() {
     xray.index()
+    this.getStartDates()
+    this.fetchAppointments()
   },
   data: function () {
     return {
+      startDate: null,
+      endDate: null,
+      filterBy: null,
+      loading: false,
+      appointments: [],
     }
   },
   methods: {
+    getStartDates() {
+      this.endDate = moment().format('YYYY-MM-DD')
+      this.startDate = moment().add(-30, 'days').format('YYYY-MM-DD')
+      this.fetchAppointments(this.startDate, this.endDate)
+    },
+    onFilterChange(value) {
+      const today = moment().format('YYYY-MM-DD')
+      if (value && value === 2) {
+        // Get Last 2 years Date from now
+        const last2YearDate = moment().subtract(2, 'years').format('YYYY-MM-DD')
+        this.endDate = today
+        this.startDate = last2YearDate
+      }
+      if (value && value !== 2) {
+        const secondDate = moment().subtract(value, 'days').format('YYYY-MM-DD')
+        this.endDate = today
+        this.startDate = secondDate
+      }
+      if (value) {
+        this.fetchAppointments(this.startDate, this.endDate)
+      }
+    },
+    onDateChange() {
+      this.fetchAppointments(this.startDate, this.endDate)
+    },
+    fetchAppointments(start, end) {
+      if (start && end) {
+        this.loading = true
+        this.appointments = []
+        getAppointmentsWithoutServices(start, end).then(response => {
+          console.log(response)
+          this.appointments = [...response]
+          this.loading = false
+        }).catch(() => {
+          this.loading = false
+        })
+      }
+    },
+    getStatus(status) {
+      if (status) {
+        return 'Attended'
+      }
+      if (!status && typeof status === 'boolean') {
+        return 'Not Attended'
+      }
+      return ''
+    },
   },
 }
 </script>
--- api/dao/daoCallCenter.js
+++ api/dao/daoCallCenter.js
@@ -10,7 +10,7 @@ const pool = new Pool({
 })
 
 const getAppointmentsWithMissingServices = (request, response, start, end, prm_client_id, scope) => {
-  let statement = "SELECT CONCAT_WS(' ', enq.name, enq.last_name) AS lead_name, CONCAT_WS(' ', us.first_name, us.surname) AS lead_owner, " +
+  let statement = "SELECT enq.id AS lead_id, CONCAT_WS(' ', enq.name, enq.last_name) AS lead_name, CONCAT_WS(' ', us.first_name, us.surname) AS lead_owner, " +
     "app.starts_at::date AS appointment_date, app.patient_attended AS appointment_status FROM appointments app "
   statement += "LEFT JOIN enquiries enq ON app.enquiry_id = enq.id "
   statement += "LEFT JOIN services se ON enq.id = se.enquiry_id "
