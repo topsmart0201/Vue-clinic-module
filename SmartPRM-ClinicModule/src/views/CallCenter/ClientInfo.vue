9a8b0158ad1ef2633d5860b19b1188721a0df87d
topsmart20 <oswellgrundmeier42@gmail.com>
2022-01-20 13:34:32 +0500

    fix: alignment and discard button color changed

--- SmartPRM-ClinicModule/src/views/CallCenter/ClientInfo.vue
+++ SmartPRM-ClinicModule/src/views/CallCenter/ClientInfo.vue
@@ -12,7 +12,7 @@
           <div v-if="!editMode" v-html="clientInfo"></div>
           <vue-editor v-if="editMode" v-model="clientInfo"></vue-editor>
           <div class="text-right mt-3" v-if="editMode">
-            <button :disabled="!clientInfo || editLoading" @click="editInfo" type="button" class="btn btn-primary mr-2">Discard</button>
+            <button :disabled="!clientInfo || editLoading" @click="editInfo" type="button" class="btn btn-secondary mr-2">Discard</button>
             <button :disabled="!clientInfo || editLoading" @click="updateClientInfo" type="button" class="btn btn-primary">Save</button>
           </div>
         </div>
